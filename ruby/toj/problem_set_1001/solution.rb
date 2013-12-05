0092006656ffbeb1c39f6d9016e78a2f4ef272f4
William Harris <wharris@upscalews.com>
2013-12-05 17:14:00 -0500

    toj pset 1001 accepted!
    http://acm.timus.ru/status.aspx?space=1&num=1001&author=me

--- ruby/toj/problem_set_1001/solution.rb
+++ ruby/toj/problem_set_1001/solution.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts  $stdin.readlines.map{|x| Math.sqrt(x.to_f).round(4)}.reverse
+puts  $stdin.readlines.map{|x| x.strip.split }.flatten.reverse.map{|x| Math.sqrt(x.to_i).round(4)}
