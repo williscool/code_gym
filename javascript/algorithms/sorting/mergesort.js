f62119e2719079eacad177749981bdb2d3959449
William Harris <wharris@upscalews.com>
2015-04-19 17:55:55 -0700

    topDown and bottomUp merge sort with tests
    topDown works. bottomUp mostly works

---
+++ javascript/algorithms/sorting/mergesort.js
@@ -0,0 +1,85 @@
+// http://en.wikipedia.org/wiki/Merge_sort
+var dsalgo = require('../../utilities.js').dsalgo;
+var swap = dsalgo.utils.swap;
+
+module.exports.mergesort = {};
+
+// inspired by http://www.nczonline.net/blog/2012/10/02/computer-science-and-javascript-merge-sort/
+// the wikipedia entry psuedocode was doing to much
+function merge(left,right) {
+  var result = [],
+      iLeft = 0,
+      iRight = 0;
+
+  while (iLeft < left.length && iRight < right.length ){
+    if (left[iLeft] < right[iRight]){
+     result.push(left[iLeft]);
+     iLeft = iLeft + 1;
+    } else {
+     result.push(right[iRight]);
+     iRight = iRight + 1;
+    }
+  }
+
+  // lol wat http://stackoverflow.com/questions/7124884/why-does-1-2-3-4-1-23-4-in-javascript
+  // return result + left.slice(iLeft) + right.slice(iRight);
+  //
+  // slice returns all the elements after and at the index you give it
+  return result.concat(left.slice(iLeft)).concat(right.slice(iRight));
+}
+
+module.exports.mergesort.topDown = function(list) {
+ var mergesort = module.exports.mergesort.topDown;	
+
+  if(list.length <= 1){
+    return list;
+  }
+
+  var left = [], 
+      right = [], 
+      middle = Math.floor(list.length / 2), 
+      len = list.length, 
+      i;
+
+  for(i = 0; i < len; i++ ) {
+    if (i < middle) {
+      left.push(list[i]);
+    } else {
+      right.push(list[i]);
+    }
+  }
+  
+  left = mergesort(left);
+  right = mergesort(right);
+
+	return merge(left,right);
+};
+
+module.exports.mergesort.bottomUp = function(list) {
+ var mergesort = module.exports.mergesort.bottomUp;	
+
+  if(list.length <= 1){
+    return list;
+  }
+
+  var len = list.length, 
+      temp = [],
+      blockSize, i;
+
+  for(blockSize = 1; blockSize < len; blockSize = (blockSize * 2)) {
+
+    for(i = 0 ; i <= len - blockSize; i = i + (blockSize * 2) ){
+      var left = list.slice(i, i + blockSize), // javascript list slice is not inclusive of the element at the end index that you give it
+        right = list.slice(i + blockSize, Math.min((i + (2 * blockSize)), len));
+
+       temp = temp.concat(merge(left,right));
+    }
+
+    list = temp;
+    temp = [];
+  }
+  
+	return list;
+}
+
+
---
+++ javascript/test/algorithms/sorting/mergesort_test.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+var MergeSort = require('../../../algorithms/sorting/mergesort.js').mergesort;
+var dsalgo = require('../../../utilities.js').dsalgo;
+
+Object.keys(MergeSort).forEach(function(key) {
+
+  var fn = MergeSort[key];
+  var mergesort = fn;
+  describe( key + ' Merge Sort', function(){
+
+    describe('should work with arrays', function () {
+      it('that are empty', function () {
+        assert.deepEqual(mergesort([]), []);
+      });
+
+      it('that are have 1 element', function () {
+        assert.deepEqual(mergesort([1]), [1]);
+      });
+
+      it('with no duplicates', function () {
+        assert.deepEqual(mergesort([9, 2, 8, 6, 1, 3]), [1, 2, 3, 6, 8, 9]);
+        assert.deepEqual(mergesort([5, 2, 2, 6, 1, 3]), [1, 2, 2, 3, 5, 6]);
+        assert.deepEqual(mergesort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
+      });
+
+      it('that have duplicates', function () {
+        assert.deepEqual(mergesort([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]), [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
+      });
+
+      it('that have negative values', function () {
+        assert.deepEqual(mergesort([0, 0, 0, 0, 0, -1]), [-1, 0, 0, 0, 0, 0]);
+      });
+
+      it('that are random and non-sorted', function () {
+        var array = dsalgo.utils.makeRandomArray({
+          precision: 0
+        });
+
+        // so this version of merge sort creates a new array
+        // and thus isnt in place and we need to reassign array to new array
+        array = mergesort(array);
+
+        for (var i = 1; i < array.length; i++) {
+          assert((array[i - 1] <= array[i]) === true);
+        }
+      });
+    });
+
+  });
+});
