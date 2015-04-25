ba6747064586f9b4c5970b97b937a4a417ab1f8c
William Harris <wharris@upscalews.com>
2015-04-25 13:14:15 -0700

    update utils to have falsy to use

--- javascript/utilities.js
+++ javascript/utilities.js
@@ -1,19 +1,31 @@
 // so things dont get dupped all over the place
 
 module.exports.dsalgo = {utils: {}}
+var utils = module.exports.dsalgo.utils;
 
 // techincally the return here is unnecessary since we never make a deepCopy of the array 
 // so its muting the original. But I just like this interface better
-module.exports.dsalgo.utils.swap = function (list, firstIndex, secondIndex) {
+utils.swap = function (list, firstIndex, secondIndex) {
   var temp = list[firstIndex];
   list[firstIndex] = list[secondIndex];
   list[secondIndex] = temp;
   return list;
 }
 
-module.exports.dsalgo.utils.makeRandomArray = function (config) {
+utils.isTruthy = function (thing) {
+  // https://github.com/jasmine/jasmine/blob/master/src/core/matchers/toBeTruthy.js
+  // http://nfriedly.com/techblog/2009/07/advanced-javascript-operators-and-truthy-falsy/
+  return !!thing;
+}
+
+utils.isFalsy = function (thing) {
+  // https://github.com/jasmine/jasmine/blob/master/src/core/matchers/toBeFalsy.js
+  return !utils.isTruthy(thing);
+}
+
+utils.makeRandomArray = function (config) {
   var conf = config || {};
-  var precision = (typeof conf.precision === undefined) ? 2 : conf.precision;
+  var precision = (utils.isFalsy(config.precision)) ? 2 : conf.precision;
   var multiplier = 100;
   var size = 100;
   var result = [];
@@ -23,3 +35,4 @@ module.exports.dsalgo.utils.makeRandomArray = function (config) {
   }
   return result;
 }
+
