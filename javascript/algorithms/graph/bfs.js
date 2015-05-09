4b89b1f30d43992f97c44ecf9c1c2b1dd29af499
William Harris <wharris@upscalews.com>
2015-05-09 16:45:21 -0700

    bfs with tests
    found the bug in my queue because of it

---
+++ javascript/algorithms/graph/bfs.js
@@ -0,0 +1,70 @@
+// http://en.wikipedia.org/wiki/Breadth-first_search
+
+// https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/a/breadth-first-search-and-its-uses
+// http://en.wikipedia.org/wiki/Breadth-first_search
+//
+// Great article on asymptotic analysis of this algorithm and graph algos in general
+// https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/a/analysis-of-breadth-first-search
+
+/*
+ * Performs a breadth-first search on a graph
+ * @param {Graph} graph - Graph object from which we access an adjancency list
+ * @returns {array} Array of objects describing each vertex, like
+ *     [{distance: _, predecessor: _ }]
+ */
+
+// this and the other graph algorithms probably make more sense as a part of the graph object
+// then their own seperate clasy things but its a nice way to keep the code seperate
+// and not make that any bigger
+
+// fun observation level order traversing a binary search tree is a depth first search
+// but for depth first search on a graph a node can be connected to more than 2 other nodes
+
+var Queue = require('../../data_structures/queue.js');
+
+module.exports = function (graph, start_vertex) {
+  
+  if (graph.order() < 1) return Error("come on dog there's no nodes in this graph.")
+
+  var info = [];
+  
+  for(var i = 0; i < graph.order() ; i++) {
+    info[i] = {distance: null, predecessor:null};
+  }
+
+  info[start_vertex].distance = 0;
+
+  var queue = new Queue();
+  queue.enqueue(start_vertex);
+
+
+  // Traverse the graph
+  
+  // As long as the queue is not empty:
+  //  Repeatedly dequeue a vertex v from the queue.
+  //  
+  //  For each neighbor w of v that has not been visited:
+  //     Set distance to 1 greater than v's distance
+  //     Set predecessor to v
+  //     Enqueue w
+
+  while(queue.length > 0) {
+
+   var v = queue.dequeue();
+  
+    for(var i = 0; i < graph.adjacency_list[v].length ; i++){
+        var w = graph.adjacency_list[v][i];
+        
+        if(info[w].distance === null){
+            
+            info[w].distance = info[v].distance + 1;
+            info[w].predecessor = v;
+            queue.enqueue(w);    
+        }
+        
+    } 
+
+  }
+
+  return info;
+};
---
+++ javascript/test/algorithms/graph/bfs_test.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+var BFS = require('../../../algorithms/graph/bfs.js');
+var Graph = require('../../../data_structures/graph.js');
+var dsalgo = require('../../../utilities.js').dsalgo;
+
+describe('Breadth First Search', function(){
+
+  describe('traverse graph', function () {
+      var adjListToo = [
+          [1],
+          [0, 4, 5],
+          [3, 4, 5],
+          [2, 6],
+          [1, 2],
+          [1, 2, 6],
+          [3, 5],
+          []
+          ];
+      
+      var graph = new Graph({
+          adjList : adjListToo
+        });
+
+      var bfsInfo = new BFS(graph,3);
+
+    it("marks source vertex's predecessor as null", function () {
+      assert.deepEqual(bfsInfo[3], {distance: 0, predecessor: null});
+    });
+
+    it('goes in correct order', function () {
+
+      assert.deepEqual(bfsInfo[0], {distance: 4, predecessor: 1});
+      assert.deepEqual(bfsInfo[1], {distance: 3, predecessor: 4});
+      assert.deepEqual(bfsInfo[2], {distance: 1, predecessor: 3});
+      assert.deepEqual(bfsInfo[4], {distance: 2, predecessor: 2});
+      assert.deepEqual(bfsInfo[5], {distance: 2, predecessor: 2});
+      assert.deepEqual(bfsInfo[6], {distance: 1, predecessor: 3});
+    });
+
+    it('doesnt mark unconnected vertex', function () {
+      assert.deepEqual(bfsInfo[7], {distance: null, predecessor: null});
+    });
+
+  });
+
+});
