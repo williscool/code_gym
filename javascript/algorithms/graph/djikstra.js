c87ff775731a6e21072e80083d4078d85d5f9cf4
William Harris <wharris@upscalews.com>
2015-05-17 16:00:41 -0700

    naive djikstra shortest path with tests

--- javascript/algorithms/graph/djikstra.js
+++ javascript/algorithms/graph/djikstra.js
@@ -3,11 +3,10 @@
 
 var dsalgo = require('../../utilities.js').dsalgo;
 
-// TODO add shortest path printing function
-//
-// then do it with proirity queues
-
-module.exports = function (graph, start_vertex, target_vertex) {
+var naiveDijkstra = function (graph, start_vertex, target_vertex) {
+  this.source = start_vertex;
+  // lightly influenced by 
+  // https://github.com/gabormakrai/dijkstra-performance/blob/master/Dijkstra.md
 
   // "Dijkstra's original algorithm does not use a min-priority queue and runs in time O(V^2)"
   //
@@ -38,7 +37,7 @@ module.exports = function (graph, start_vertex, target_vertex) {
   var findMinIndex = function(array, vertexHash){
 
     var lowest = Number.POSITIVE_INFINITY;
-    var index = -1
+    var index = -1;
 
     array.forEach(function(val, i){
       // if this vertex has not been visited and the distance is the new lowest
@@ -92,5 +91,75 @@ module.exports = function (graph, start_vertex, target_vertex) {
 
   }
 
-  return info;
+  this.info = info;
 };
+
+naiveDijkstra.prototype.shortest_path = function(target_vertex) {
+
+  if(!this.info) throw new Error("dude I need a graph traversal to work with");
+
+   if(!this.info[target_vertex]){
+      // vertex isn't in the list period so there is no shortest path to it
+      return false;
+   }
+
+   var currVertInfo = this.info[target_vertex];
+   // need to keep track of the vertex in the list. 
+   // it wont be at its corresponding index in the array
+   //
+   // i.e. {vertex:6, distance: "1.51", predecessor: 3}
+   //
+   // also js is pass by reference so it would mute the orginal info object anyway unless we deep cloned
+   // so might as well just embrace that and delete it later
+   
+   currVertInfo.vertex = target_vertex;
+
+   var list = [];
+
+   // would have used unshift but its O(n) each time
+   // http://stackoverflow.com/questions/1590247/how-do-you-implement-a-stack-and-a-queue-in-javascript#comment1453625_1590262
+   // https://github.com/v8/v8/blob/master/src/array.js#L662
+   //
+   // faster to just reverse the array at the end
+
+   while(currVertInfo) {
+
+     list.push(currVertInfo); 
+
+     // need this break for the case of having added the source who has no predecessor
+     //
+     // ahem. in js 0 == false so if zero is a vertex 
+     // that can be checked that could fuck things up with out a proper defined check
+     
+     if(currVertInfo.predecessor === null) break;
+
+     var nextVertInfo = this.info[currVertInfo.predecessor];
+     nextVertInfo.vertex = currVertInfo.predecessor;
+
+     currVertInfo = nextVertInfo;
+   }
+
+   // if there is no path to source return false
+   // just checks if the last vertex we end up at is the source or not
+   if(currVertInfo.vertex !== this.source) return false;
+
+   // reverse our array as it is in backwards order since we added to end of array and not beginning
+   list.reverse();
+
+   var path_info = dsalgo.utils.simpleSet();
+
+   path_info.order = [];
+
+   // unforunately now this will all go to hell if there is a vertex named order 
+   // but for our academic purposes of just learning how this algorithm works that is ok
+   list.forEach(function(val){
+      var vert = val.vertex;
+      delete val.vertex;
+      path_info.order.push(vert);
+      path_info[vert] = val;
+   });
+
+   return path_info;
+};
+
+module.exports.naive = naiveDijkstra;
