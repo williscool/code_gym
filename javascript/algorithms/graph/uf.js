55720bff948eaa6abc3727f2dd9e4b730aa6c8c1
William Harris <wharris@upscalews.com>
2015-06-05 19:54:47 -0700

    quick union union find and tests

--- javascript/algorithms/graph/uf.js
+++ javascript/algorithms/graph/uf.js
@@ -64,7 +64,70 @@ quickFindUF.prototype.union = function(p,q){
   return this;
 }
 
-quickFindUF.prototype.add_from_text = function(list){
+// http://algs4.cs.princeton.edu/15uf/images/quick-union-overview.png
+function quickUnionUF(thingy) {
+
+  // this data structure / algorithm sucks it could devolve both find and union into O(n) very quickly for densely connected components if a connection tree gets large
+  // probably useful to keep a seperate data structure with the roots of trees so you can access those in constant time
+  // but fine for our academic purposes
+  
+  if(!dsalgo.utils.isDefined(thingy)) throw new Error("need number of sites or a connections string to properly initalize parent array");
+
+  var N,connections;
+
+  if(typeof thingy === "number"){
+    N = thingy;
+  } else {
+    connections = thingy; 
+  }
+
+  this.parent = [];
+  if(N) this.initalize_component_array(N);
+  if(connections) this.add_from_text(connections);
+}
+
+quickUnionUF.prototype.initalize_component_array = function(N){
+  // init empty UF data structure with N isolated components
+  for(var i=0; i < N; i++) this.parent[i] = i;
+  this.count = N;
+}
+
+quickUnionUF.prototype.validate = function(p){
+  var num_sites = this.parent.length;
+  if(p < 0 || p > num_sites) throw new Error("site " + p + " is not a valid site. as it is not between 0 and the number of sites which is " + num_sites);
+}
+
+quickUnionUF.prototype.find = function(p){
+  // returns the id for the component contanining site p
+  // obviously O(n)
+
+  this.validate(p);
+  while(p != this.parent[p]){
+    p = this.parent[p];
+  }
+  
+  return p;
+}
+
+
+quickUnionUF.prototype.connected = function(p,q){
+  return this.find(p) === this.find(q);
+}
+
+quickUnionUF.prototype.union = function(p,q){
+
+  var rootP = this.find(p);
+  var rootQ = this.find(q);
+
+  if( rootP === rootQ ) return;
+
+  this.parent[rootP] = rootQ;
+
+  this.count = this.count - 1;
+  return this;
+}
+
+var add_site_from_text = function(list){
   // i.e. http://algs4.cs.princeton.edu/15uf/tinyUF.txt
   
   var context = this;
@@ -85,7 +148,10 @@ quickFindUF.prototype.add_from_text = function(list){
   });
 }
 
+quickFindUF.prototype.add_from_text = add_site_from_text;
+quickUnionUF.prototype.add_from_text = add_site_from_text;
 
 module.exports = {
-  quick_find: quickFindUF 
-}
+  quick_find: quickFindUF,
+  quick_union: quickUnionUF 
+};
--- javascript/test/algorithms/graph/uf_test.js
+++ javascript/test/algorithms/graph/uf_test.js
@@ -1,54 +1,56 @@
 var assert = require('assert');
-var UF = require('../../../algorithms/graph/uf.js').quick_find;
+var UF = require('../../../algorithms/graph/uf.js');
 var dsalgo = require('../../../utilities.js').dsalgo;
 
 // http://algs4.cs.princeton.edu/15uf/images/dynamic-connectivity-tiny.png
 var connections = dsalgo.utils.requireText(__dirname, '../../../data/graph/tinyUF.txt');
 
-describe('Union Find', function(){
+Object.keys(UF).forEach(function(key){
+  describe(key + ' Union Find', function(){
 
-  var simpleUf;
+    var simpleUf;
 
-  it("initalizes", function () {
-    simpleUf = new UF(2);
+    it("initalizes", function () {
+      simpleUf = new UF[key](2);
+    });
+
+    it("count", function () {
+      assert.equal(simpleUf.count, 2);
+    });
+
+    it('find', function () {
+      assert.equal(simpleUf.find(1), 1);
+      assert.throws(function(){
+        simpleUf.find(20);
+      }, /not a valid site/);
+    });
+
+    it('connected', function () {
+      assert.equal(simpleUf.connected(1,1), true);
+      assert.equal(simpleUf.connected(1,2), false);
+    });
+
+    it('union', function () {
+      simpleUf.union(1,2);
+      assert.equal(simpleUf.connected(1,2), true);
+      assert.equal(simpleUf.count, 1);
+    });
+
+    it('imports connection file correctly', function () {
+      var importedUF = new UF[key](connections);
+      assert.equal(importedUF.connected(4,3), true);
+      assert.equal(importedUF.connected(3,8), true);
+      assert.equal(importedUF.connected(6,5), true);
+      assert.equal(importedUF.connected(9,4), true);
+      assert.equal(importedUF.connected(2,1), true);
+      assert.equal(importedUF.connected(8,9), true);
+      assert.equal(importedUF.connected(5,0), true);
+      assert.equal(importedUF.connected(7,2), true);
+      assert.equal(importedUF.connected(6,1), true);
+      assert.equal(importedUF.connected(1,0), true);
+      assert.equal(importedUF.connected(6,7), true);
+      assert.equal(importedUF.count, 2);
+    });
+
   });
-
-  it("count", function () {
-    assert.equal(simpleUf.count, 2);
-  });
-
-  it('find', function () {
-    assert.equal(simpleUf.find(1), 1);
-    assert.throws(function(){
-      simpleUf.find(20);
-    }, /not a valid site/);
-  });
-
-  it('connected', function () {
-    assert.equal(simpleUf.connected(1,1), true);
-    assert.equal(simpleUf.connected(1,2), false);
-  });
-
-  it('union', function () {
-    simpleUf.union(1,2);
-    assert.equal(simpleUf.connected(1,2), true);
-    assert.equal(simpleUf.count, 1);
-  });
-
-  it('imports connection file correctly', function () {
-    var importedUF = new UF(connections);
-    assert.equal(importedUF.connected(4,3), true);
-    assert.equal(importedUF.connected(3,8), true);
-    assert.equal(importedUF.connected(6,5), true);
-    assert.equal(importedUF.connected(9,4), true);
-    assert.equal(importedUF.connected(2,1), true);
-    assert.equal(importedUF.connected(8,9), true);
-    assert.equal(importedUF.connected(5,0), true);
-    assert.equal(importedUF.connected(7,2), true);
-    assert.equal(importedUF.connected(6,1), true);
-    assert.equal(importedUF.connected(1,0), true);
-    assert.equal(importedUF.connected(6,7), true);
-    assert.equal(importedUF.count, 2);
-  });
-
 });
