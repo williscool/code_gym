bbfda0f5915e684af0d6576ca4f64024f3809f9c
William Harris <wharris@upscalews.com>
2015-06-23 16:33:33 -0700

    function that calculates factorial

---
+++ javascript/math/factorial.js
@@ -0,0 +1,12 @@
+module.exports = function(n) {
+
+  if(n == 0) return 1;
+
+  var result = n;
+
+  for(var i = n - 1; i > 0; i-- ){
+    result = result * i; 
+  }
+
+  return result;
+};
---
+++ javascript/test/math/factorial_test.js
@@ -0,0 +1,14 @@
+// inspired by: https://github.com/josdejong/mathjs/blob/master/test/function/probability/factorial.test.js
+
+var assert = require('assert');
+var factorial = require('../../math/factorial.js');
+describe("Factorial",function(){
+  it("shoiuld calculate the factorial of a number", function(){
+    assert.equal(factorial(0), 1);
+    assert.equal(factorial(1), 1);
+    assert.equal(factorial(2), 2);
+    assert.equal(factorial(3), 6);
+    assert.equal(factorial(4), 24);
+    assert.equal(factorial(5), 120);
+  });
+});
