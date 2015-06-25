671c9f63440276dfc74b8967acf8b3a3bf73223a
William Harris <wharris@upscalews.com>
2015-06-24 23:10:18 -0700

    combinations with replacement and tests

--- javascript/math/combinatorics/combinations.js
+++ javascript/math/combinatorics/combinations.js
@@ -6,8 +6,19 @@ var permutations = require('./permutations.js').without_repetition;
 var factorial = require('../factorial.js');
 
 // NOTE: http://math.stackexchange.com/questions/551920/what-is-zero-choose-one/551926#551926
-module.exports = function(n, k) {
+function withoutRep(n, k) {
   if(n === 0 && k === 0) return 1;
   if(k === 0) return 0;
   return permutations(n,k) / factorial(k);
+}
+
+/// combo with_repetition how do they work? https://www.youtube.com/watch?v=ZcSSI6VY1kM
+function withRep(n,k){
+  if(n === 0 && k === 0) return 1;
+  return withoutRep(n + k - 1, k);
+}
+
+module.exports = {
+  without_repetition: withoutRep,
+  with_repetition: withRep
 }
