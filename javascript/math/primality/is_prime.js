df53f878fd884811debb5db7d91a77349af226f1
William Harris <wharris@upscalews.com>
2015-07-01 19:43:22 -0700

    optimized isPrime and tests

--- javascript/math/primality/is_prime.js
+++ javascript/math/primality/is_prime.js
@@ -1,8 +1,7 @@
 // https://en.wikipedia.org/wiki/Primality_test#Simple_methods
-// aka trial division
 
 // inspired by: http://www.thatjsdude.com/interview/js1.html#isPrime
-function Naive(n) {
+function naiveTrialDivision(n) {
   //
   // why 0 isnt prime
   // http://mathforum.org/library/drmath/view/57036.html
@@ -31,6 +30,46 @@ function Naive(n) {
   return true;
 };
 
-module.exports =  {
-  naive: Naive 
+// inspried by http://www.thatjsdude.com/interview/js1.html#primeExtra
+// https://github.com/mgechev/javascript-algorithms/blob/master/src/primes/is-prime.js
+// https://en.wikipedia.org/wiki/Primality_test#Pseudocode
+//
+// basically the improvements to this test boil down to the intuition that if you can't evenly divide the number we are testing by 2. 
+// clearly you only need to look at the numbers lower than half of its value. 
+//
+// If you can't evenly divide it by 3 you only need to test numbers under 1/3 of its value... and so on and so forth
+//
+// from the wikipedia article:
+// The algorithm can be improved further by observing that all primes are of the form 6k ± 1, with the exception of 2 and 3. 
+// This is because all integers can be expressed as (6k + i) for some integer k and for i = -1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4); and 3 divides (6k + 3). 
+// So a more efficient method is to test if n is divisible by 2 or 3, then to check through all the numbers of form 6k ± 1 <= sqrt(n).
+//
+function optimizedTrialDivision (n) {
+  
+  if (n < 2) return false;
+
+  if (n <= 3) return true;
+
+  if( (n % 2 == 0) || (n % 3 == 0) ) return false;
+
+  // divisor * divisor <= n
+  var divisor = 5;
+  var limit = Math.floor(Math.sqrt(n));
+
+  while(divisor <= limit){
+    if( (n % divisor == 0) || ( n % (divisor + 2) == 0) ) return false;
+    divisor += 6;
+  }
+  
+  return true;
+}
+
+// more versions 
+// https://www.topcoder.com/community/data-science/data-science-tutorials/primality-testing-non-deterministic-algorithms/
+// https://en.wikipedia.org/wiki/Primality_test#Probabilistic_tests
+// https://en.wikipedia.org/wiki/Primality_test#Fast_deterministic_tests
+
+module.exports = {
+  naive_trial_division: naiveTrialDivision,
+  optimized_trial_division: optimizedTrialDivision 
 };
