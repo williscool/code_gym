7bef781cf55bc82cca5000f49d1640b217b6c2ae
William Harris <wharris@upscalews.com>
2015-07-04 12:24:10 -0700

    all you favorite permuations of nth fib wit tests

--- javascript/algorithms/dynamic_programming/nth_fibbonacci.js
+++ javascript/algorithms/dynamic_programming/nth_fibbonacci.js
@@ -1,14 +1,57 @@
 // https://en.wikipedia.org/wiki/Dynamic_programming#Fibonacci_sequence
-function naive(n){
+var dsalgo = require('../../utilities.js').dsalgo;
+  
+function naiveRecursive(n){
   if (n === 0) return 0;
   if (n === 1) return 1;
-  return naive(n - 2) + naive(n - 1);
+  return naiveRecursive(n - 2) + naiveRecursive(n - 1);
 }
 
-// want to do both iterative and recursive versions
-// and of course memoized and not
+function memoizedRecursive(n) {
+  var cache = [0,1];
+
+  if(!dsalgo.utils.isDefined(cache[n])){
+   cache[n] = naiveRecursive(n - 2) + naiveRecursive(n - 1);
+  }
+
+  return cache[n];
+}
+
+function iterative(n){
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  var sequence = [0,1];
+
+  for(var i = 2; i <= n; i++ ) {
+    sequence[i] = sequence[i-2] + sequence[i - 1];
+  }
+  
+  return sequence[n];
+}
+
+// somewhat inspired by
+// https://github.com/mgechev/javascript-algorithms/blob/master/src/others/fibonacci.js
+// http://stackoverflow.com/questions/13440020/big-o-for-various-fibonacci-implementations
+function iterativeConstantSpace(n){
+  var n1 = 0;
+  var n2 = 1;
+  var temp;
+
+
+  for(var i = 1; i <= n; i++ ) {
+    temp = n1 + n2;
+    n1 = n2;
+    n2 = temp;
+  }
+  
+  return n1;
+}
+
 
 module.exports = {
-  naive: naive //,
-//  memoized: Memoized 
+  naive_recursive: naiveRecursive,
+  iterative: iterative,
+  iterative_constant_space: iterativeConstantSpace,
+  memoized_recursive: memoizedRecursive 
 };
--- javascript/test/algorithms/dynamic_programming/nth_fibbonacci_test.js
+++ javascript/test/algorithms/dynamic_programming/nth_fibbonacci_test.js
@@ -1,13 +1,17 @@
 var assert = require('assert');
-var fibbonacci = require('../../../algorithms/dynamic_programming/nth_fibbonacci.js').naive;
+var fibbonacciFns = require('../../../algorithms/dynamic_programming/nth_fibbonacci.js');
 
-describe("nth fibbonacci",function(){
-  it("shoiuld calculate nth fibbonacci number", function(){
-    assert.equal(fibbonacci(0), 0);
-    assert.equal(fibbonacci(1), 1);
-    assert.equal(fibbonacci(2), 1);
-    assert.equal(fibbonacci(3), 2);
-    assert.equal(fibbonacci(4), 3);
-    assert.equal(fibbonacci(5), 5);
+Object.keys(fibbonacciFns).forEach(function(key){
+  describe( key + " nth fibbonacci",function(){
+    var fibbonacci = fibbonacciFns[key];
+    it("shoiuld calculate nth fibbonacci number", function(){
+      assert.equal(fibbonacci(0), 0);
+      assert.equal(fibbonacci(1), 1);
+      assert.equal(fibbonacci(2), 1);
+      assert.equal(fibbonacci(3), 2);
+      assert.equal(fibbonacci(4), 3);
+      assert.equal(fibbonacci(5), 5);
+      assert.equal(fibbonacci(12), 144);
+    });
   });
 });
