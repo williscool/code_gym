c36c464978c5a8407d66d717c50230f4e1f1fe00
William Harris <wharris@upscalews.com>
2015-11-16 22:42:17 -0600

    iterative rod cutting
    also style fixes

--- javascript/algorithms/dynamic_programming/rod_cutting.js
+++ javascript/algorithms/dynamic_programming/rod_cutting.js
@@ -8,12 +8,12 @@ var dsalgo = require('../../utilities.js').dsalgo;
 // https://www.quora.com/How-does-one-obtain-the-recursive-formula-for-the-Rod-Cutting-Problem
 function naiveRecursiveRodCutting(values, n)  {
 
-  if (n == 0) return 0;
+  if (n === 0) return 0;
    
   var max = Number.NEGATIVE_INFINITY;
   
   for(var i = 0; i < n; i++) {
-    max = Math.max(max, values[i] + naiveRecursiveRodCutting(values, n - i - 1))
+    max = Math.max(max, values[i] + naiveRecursiveRodCutting(values, n - i - 1));
   }
 
   return max;
@@ -22,23 +22,38 @@ function naiveRecursiveRodCutting(values, n)  {
 // memoized version
 function rRodCutting(values, n , memo)  {
 
-  if (n == 0) return 0;
+  if (n === 0) return 0;
   if (dsalgo.utils.isDefined(memo[n])) return memo[n];
    
   var max = Number.NEGATIVE_INFINITY;
   
   for(var i = 0; i < n; i++) {
-   memo[n] = Math.max(max, values[i] + rRodCutting(values, n - i - 1, memo))
+   memo[n] = Math.max(max, values[i] + rRodCutting(values, n - i - 1, memo));
   }
 
   return memo[n];
 }
 
-function iRodCutting(arr) {
+function iRodCutting(values) {
+  // zero cut has zero value you
+  var memo = [0];
+  var n = values.length;
+
+  var max;
+
+  for(var i = 1; i <= n; i++) {
+    max = Number.NEGATIVE_INFINITY;
+    for(var j = 0; j < i; j++) {
+       max = Math.max(max, values[j] + memo[i - j - 1]);
+       memo[i] = max;
+    }
+  }
+
+  return memo[n];
 }
 
 module.exports = {
-  // iterative: iLCS,
-  recursive: function (values) {return naiveRecursiveRodCutting(values, values.length, [])},
-  naive_recursive: function (values) {return naiveRecursiveRodCutting(values, values.length)}
+  iterative: iRodCutting,
+  recursive: function (values) {return naiveRecursiveRodCutting(values, values.length, []);},
+  naive_recursive: function (values) {return naiveRecursiveRodCutting(values, values.length);}
 };
--- javascript/test/algorithms/dynamic_programming/rod_cutting_test.js
+++ javascript/test/algorithms/dynamic_programming/rod_cutting_test.js
@@ -5,5 +5,6 @@ Object.keys(RCfns).forEach(function(key) {
   it(key +  " Rod Cutting Problem", function() {
     var rodCutting = RCfns[key];
     assert.deepEqual(rodCutting([1, 5, 8, 9, 10, 17, 17, 20]), 22 );
+    assert.deepEqual(rodCutting([0,1, 5, 8, 9, 10, 17, 17, 20, 24, 30]), 30 );
   });
 });
