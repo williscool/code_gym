c993097b1e9d0e8b23c2515ac360082412e5946c
William Harris <wharris@upscalews.com>
2015-11-22 18:39:06 -0600

    fix wrong test name

--- javascript/test/algorithms/dynamic_programming/edit_distance_test.js
+++ javascript/test/algorithms/dynamic_programming/edit_distance_test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var eDistFns = require('../../../algorithms/dynamic_programming/edit_distance.js');
 
 Object.keys(eDistFns).forEach(function(key) {
-  it(key +  " Longest Common Subsequence", function() {
+  it(key +  " Edit Distance", function() {
     var editDistance = eDistFns[key];
     assert.deepEqual(editDistance ("sunday","saturday"), 3 );
   });
