372b211faca1a5b49a70729f61d3e3cbbb9d872b
William Harris <wharris@upscalews.com>
2015-12-03 19:40:01 -0600

    fix wrong test name

--- javascript/test/algorithms/string_search/boyer_moore_test.js
+++ javascript/test/algorithms/string_search/boyer_moore_test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var boyerMoore = require('../../../algorithms/string_search/boyer_moore.js');
 
-describe('KMP String Search', function() {
+describe('Boyer Moore String Search', function() {
   it('works with empty strings', function() {
     // boyerMoore more is special in that it calculating the bad char arr of an empty string will find it returning 0 for another empty string lol
     // dont know if I should handle as special case or if that is intended behavior
