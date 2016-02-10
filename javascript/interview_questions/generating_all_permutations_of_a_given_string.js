d5d13216be1c4b2ee67925dce532049d637fa067
William Harris <wharris@upscalews.com>
2016-02-09 18:49:01 -0600

    all permutations of an array

--- javascript/interview_questions/generating_all_permutations_of_a_given_string.js
+++ javascript/interview_questions/generating_all_permutations_of_a_given_string.js
@@ -21,10 +21,39 @@ function recursiveStringPermutations(prefix, string, stop){
   return permutations;
 }
 
+// permutations of an array
+function permutations(array){
+  
+  if (array.length === 0) return [[]];
+  
+  var perms = [];
+  
+  for(var i = 0; i < array.length; i++) {
+    
+    var copy = array.slice(0);
+    var prefix = copy.splice(i,1);
+
+    var rest = permutations(copy);
+    
+    for(var j = 0; j < rest.length ; j++) {
+      perms.push(prefix.concat(rest[j])); 
+    }
+    
+  }
+  
+  return perms;
+}
+
 module.exports = {
-  recursive: function (str, len) { 
+  recursive_constant_space: function (str, len) { 
     var stop = 0;
     if(dsalgo.utils.isDefined(len)) stop = len - 1;
     return recursiveStringPermutations("", str, stop) ;
+  },
+
+  recursive: function(str, len){
+    return permutations(str.split("")).map(function(arr){
+      return arr.join("")
+    }); 
   }
 };
