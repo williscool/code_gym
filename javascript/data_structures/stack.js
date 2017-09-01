fd0bc07f57c13f1b7fca2ec906a3713c7a57aa86
William Harris <wharris@upscalews.com>
2017-08-31 20:22:11 -0700

    es6ify stack

--- javascript/data_structures/stack.js
+++ javascript/data_structures/stack.js
@@ -1,58 +1,117 @@
-function stackWithArray() {
-  this.items = [];
-  this.stack_top_index = -1;
-  this.push = function(thing) {
+import LinkedList from './linked_list/singly_linked_list';
+
+/**
+ *
+ * Build a stack using a javascript array
+ * @class stackWithArray
+ *
+ */
+class stackWithArray {
+  constructor() {
+    this.items = [];
+    this.stack_top_index = -1;
+  }
+
+  /**
+   * Push a new thing into the stack
+   *
+   * @param {any} thing
+   * @returns this
+   * @memberof stackWithArray
+   */
+  push(thing) {
     this.stack_top_index = this.stack_top_index + 1;
     this.items[this.stack_top_index] = thing;
     return this;
-  };
+  }
 
-  this.pop = function() {
+  /**
+   * Pops the thing on the top of the stack
+   *
+   * @param {any} topItem
+   * @memberof stackWithArray
+   */
+  pop() {
+    let topItem;
     if (this.stack_top_index > -1) {
-      var top_item = this.items[this.stack_top_index];
+      topItem = this.items[this.stack_top_index];
       this.items.splice(this.stack_top_index);
       // http://stackoverflow.com/questions/500606/javascript-array-delete-elements/500617#500617
-      this.stack_top_index--;
-      return top_item;
+      this.stack_top_index -= 1;
     } else {
-      throw Error("stack empty");
+      throw Error('stack empty');
     }
-  };
 
-  this.isEmpty = function() {
+    return topItem;
+  }
+
+  /**
+   * Check if the stack is empty
+   *
+   * @returns {bool} isEmpty
+   * @memberof stackWithArray
+   */
+  isEmpty() {
     return this.stack_top_index === -1;
-  };
+  }
 }
 
+/**
+ *
+ * Implements a stack with a linked list
+ *
+ * @class stackWithLinkedList
+ */
+class stackWithLinkedList {
+  constructor() {
+    this.items = new LinkedList();
+    this.stack_top_index = -1;
+  }
 
-var LinkedList = require('./linked_list/singly_linked_list.js');
-
-function stackWithLinkedList() {
-  this.items = new LinkedList();
-  this.stack_top_index = -1;
-  this.push = function(thing) {
+  /**
+   * Push a new thing into the stack
+   *
+   * @param {any} thing
+   * @returns this
+   * @memberof stackWithLinkedList
+   */
+  push(thing) {
     this.stack_top_index = this.stack_top_index + 1;
     this.items.insertFront(thing);
     return this;
-  };
+  }
 
-  this.pop = function() {
+  /**
+   * Pops the thing on the top of the stack
+   *
+   * @param {any} topItem
+   * @memberof stackWithLinkedList
+   */
+  pop() {
+    let topItem;
     if (this.stack_top_index > -1) {
-      var top_item = this.items.head.value;
+      topItem = this.items.head.value;
       this.items.removeFront();
-      this.stack_top_index--;
-      return top_item;
+      this.stack_top_index -= 1;
     } else {
-      throw Error("stack empty");
+      throw Error('stack empty');
     }
-  };
 
-  this.isEmpty = function() {
+    return topItem;
+  }
+
+  /**
+   * Check if the stack is empty
+   *
+   * @returns {bool} isEmpty
+   * @memberof stackWithLinkedList
+   */
+  isEmpty() {
     return this.stack_top_index === -1;
-  };
+  }
 }
 
-module.exports = {
+export default {
   array: stackWithArray,
-  linked_list: stackWithLinkedList
+  linked_list: stackWithLinkedList,
 };
--- javascript/test/data_structures/stack_test.js
+++ javascript/test/data_structures/stack_test.js
@@ -1,53 +1,49 @@
 import assert from 'assert';
 import Stack from '../../data_structures/stack';
 
-Object.keys(Stack).forEach(function(key) {
-
-  describe('Stack with ' + key, function() {
-
-    describe('#push()', function() {
-      var stack = new Stack[key]();
-      it("stack has one item on it", function() {
+Object.keys(Stack).forEach((key) => {
+  describe(`Stack with ${key}`, () => {
+    describe('#push()', () => {
+      const stack = new Stack[key]();
+      it('stack has one item on it', () => {
         stack.push(42);
         assert.equal(0, stack.stack_top_index);
       });
     });
 
-    describe('#pop()', function() {
-      var stack = new Stack[key]();
+    describe('#pop()', () => {
+      const stack = new Stack[key]();
       stack.push(42);
-      var num = stack.pop();
+      const num = stack.pop();
 
-      it("correct number is returned from stack", function() {
+      it('correct number is returned from stack', () => {
         assert.equal(42, num);
       });
 
-      it("zero items on stack", function() {
+      it('zero items on stack', () => {
         assert.equal(-1, stack.stack_top_index);
       });
 
-      it("works with more than one item on the stack", function() {
+      it('works with more than one item on the stack', () => {
         stack.push(42).push(50).push(75);
         assert.equal(stack.pop(), 75);
         assert.equal(stack.pop(), 50);
       });
     });
 
-    describe('#isEmpty()', function() {
-      it("returns stack has one item on it", function() {
-        var stack = new Stack[key]();
+    describe('#isEmpty()', () => {
+      it('returns stack has one item on it', () => {
+        const stack = new Stack[key]();
         stack.push(42).push(50);
         assert.equal(false, stack.isEmpty());
       });
-      it("returns false when stack is empty", function() {
-        var stack = new Stack[key]();
+      it('returns false when stack is empty', () => {
+        const stack = new Stack[key]();
         stack.push(42).push(50);
         stack.pop();
         stack.pop();
         assert.equal(true, stack.isEmpty());
       });
     });
-
   });
-
 });
