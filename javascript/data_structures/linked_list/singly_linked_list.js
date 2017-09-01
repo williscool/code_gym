aeb7790e8a95ece499894b53240969229ba2696b
William Harris <wharris@upscalews.com>
2017-08-31 23:44:38 -0700

    es6ify singly_linked_list

--- javascript/data_structures/linked_list/singly_linked_list.js
+++ javascript/data_structures/linked_list/singly_linked_list.js
@@ -1,85 +1,133 @@
-function LinkedList() {
-  this.head = null;
-}
-
-LinkedList.prototype.insertFront = function(val) {
-  var node = {
-    value: val,
-    next: null
-  };
-
-  if (!this.head) {
-    this.head = node;
-  } else {
-    node.next = this.head;
-    this.head = node;
+/**
+ * Implements a Singly Linked List
+ * @class LinkedList
+ */
+class LinkedList {
+  constructor() {
+    this.head = null;
   }
 
-  return this;
-};
+  /**
+   * Inserts an item at the front of the list linked list
+   *
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
+   *
+   * @param {any} val
+   * @returns this
+   * @memberof LinkedList
+   */
+  insertFront(val) {
+    const node = {
+      value: val,
+      next: null,
+    };
 
-LinkedList.prototype.insertEnd = function(val) {
-  var node = {
-    value: val,
-    next: null
-  };
-
-  if (!this.head) {
-    this.head = node;
-  } else {
-    var cur = this.head;
-
-    while (cur.next) {
-      cur = cur.next;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
-    cur.next = node;
+
+    return this;
   }
 
-  return this;
-};
+  /**
+   * Insert an item at the end of the linked list
+   *
+   * Time Complexity: O(n)
+   * Space Complexity: O(1)
+   *
+   * @param {any} val
+   * @returns this
+   * @memberof LinkedList
+   */
+  insertEnd(val) {
+    const node = {
+      value: val,
+      next: null,
+    };
 
-LinkedList.prototype.removeFront = function() {
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let cur = this.head;
 
-  if (!this.head) {
+      while (cur.next) {
+        cur = cur.next;
+      }
+      cur.next = node;
+    }
+
     return this;
-  } else {
-    var new_head = this.head.next;
+  }
+
+  /**
+   * Remove an item from the front of the linked list
+   *
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
+   *
+   * @returns this
+   * @memberof LinkedList
+   */
+  removeFront() {
+    if (!this.head) {
+      return this;
+    }
+    const newHead = this.head.next;
     delete this.head;
-    this.head = new_head;
+    this.head = newHead;
     return this;
   }
-};
 
-LinkedList.prototype.removeEnd = function() {
-
-  if (!this.head) {
-    // do thing but return for chaining
-    return this;
-  } else {
-    var cur = this.head;
+  /**
+   * Remove and item from the back of the linked list
+   *
+   * Time Complexity: O(n)
+   *
+   * @returns this
+   * @memberof LinkedList
+   */
+  removeEnd() {
+    if (!this.head) {
+      // do nothing but return for method chaining
+      return this;
+    }
+    let cur = this.head;
 
     while (cur) {
       cur = cur.next;
     }
 
     cur = null;
+
+    return this;
   }
 
-  return this;
-};
+  /**
+   * Make an array representation of the linked list
+   * makes testing life easier
+   *
+   * Time Complexity: O(n)
+   * Space Complexity: O(n)
+   *
+   * @returns this
+   * @memberof LinkedList
+   */
+  toArray() {
+    const arr = [];
 
-LinkedList.prototype.toArray = function() {
-  var arr = [];
+    let cur = this.head;
 
-  var cur = this.head;
+    while (cur) {
+      arr.push(cur.value);
+      cur = cur.next;
+    }
 
-  while (cur) {
-    arr.push(cur.value);
-    cur = cur.next;
+    return arr;
   }
+}
 
-  return arr;
-};
-
-// http://stackoverflow.com/questions/9893592/node-js-object-is-not-a-function-module-exports
-module.exports = LinkedList;
+export default LinkedList;
--- javascript/test/data_structures/singly_linked_list_test.js
+++ javascript/test/data_structures/singly_linked_list_test.js
@@ -1,24 +1,23 @@
-var assert = require('assert');
-var LinkedList = require('../../data_structures/linked_list/singly_linked_list.js');
+import assert from 'assert';
+import LinkedList from '../../data_structures/linked_list/singly_linked_list';
 
-describe('Singly Linked List', function() {
-
-  describe('#insertFront()', function() {
-    var llist = new LinkedList();
-    it("inserts into linked list", function() {
+describe('Singly Linked List', () => {
+  describe('#insertFront()', () => {
+    const llist = new LinkedList();
+    it('inserts into linked list', () => {
       llist.insertFront(1);
       assert.equal(llist.head.value, 1);
     });
 
-    it("inserts subsequent ahead of older value", function() {
+    it('inserts subsequent ahead of older value', () => {
       llist.insertFront(2);
       assert.equal(llist.head.value, 2);
     });
   });
 
-  describe('#toArray()', function() {
-    var llist = new LinkedList();
-    it("converts list to an array", function() {
+  describe('#toArray()', () => {
+    const llist = new LinkedList();
+    it('converts list to an array', () => {
       llist.insertFront(1).insertFront(2);
 
       // http://stackoverflow.com/questions/13225274/the-difference-between-assert-equal-and-assert-deepequal-in-javascript-testing-w
@@ -26,44 +25,41 @@ describe('Singly Linked List', function() {
     });
   });
 
-  describe('#insertEnd()', function() {
-    var llist = new LinkedList();
+  describe('#insertEnd()', () => {
+    const llist = new LinkedList();
 
-    it("inserts into linked list", function() {
+    it('inserts into linked list', () => {
       llist.insertEnd(1);
       assert.equal(llist.head.value, 1);
     });
 
-    it("inserts subsequent value behind older value", function() {
+    it('inserts subsequent value behind older value', () => {
       llist.insertEnd(2);
       assert.equal(llist.head.value, 1);
     });
 
-    it("correctly converts toArray", function() {
+    it('correctly converts toArray', () => {
       assert.deepEqual(llist.toArray(), [1, 2]);
     });
   });
 
-  describe('#removeFront()', function() {
-    var llist = new LinkedList();
+  describe('#removeFront()', () => {
+    const llist = new LinkedList();
     llist.insertFront(1).insertFront(2);
 
-    it("removes most recently inserted value", function() {
+    it('removes most recently inserted value', () => {
       llist.removeFront();
       assert.equal(llist.head.value, 1);
     });
   });
 
-  describe('#removeEnd()', function() {
-    var llist = new LinkedList();
+  describe('#removeEnd()', () => {
+    const llist = new LinkedList();
     llist.insertFront(1).insertFront(2);
 
-    it("removes least recently inserted value", function() {
+    it('removes least recently inserted value', () => {
       llist.removeEnd();
       assert.equal(llist.head.value, 2);
     });
   });
-
-
 });
-
