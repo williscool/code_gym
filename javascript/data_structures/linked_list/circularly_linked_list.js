c9d121583e18c7555b0c3b4909a9ee86e1d104fc
William Harris <wharris@upscalews.com>
2017-09-01 01:16:55 -0700

    es6ify circularly_linked_list

--- javascript/data_structures/linked_list/circularly_linked_list.js
+++ javascript/data_structures/linked_list/circularly_linked_list.js
@@ -1,108 +1,144 @@
-// TODO: add insertAt() and removeAt()
-var DoublyLinkedList = require('../../data_structures/linked_list/doubly_linked_list.js');
+/**
+ * Implements a doubly circularly linked list
+ *
+ * This version of a linked list is a version of a doubly linked list
+ * where each node has links in both directions.
+ *
+ * https://en.wikipedia.org/wiki/Doubly_linked_list#Circular_doubly_linked_list
+ * @TODO add insertAt() and removeAt()
+ * @class CircularlyLinkedList
+ */
+class CircularlyLinkedList {
+  /**
+   * Inserts an item at the front of the list linked list
+   *
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
+   *
+   * @param {any} val
+   * @returns this
+   * @memberof CircularlyLinkedList
+   */
+  insertFront(val) {
+    const node = {
+      value: val,
+      prev: null,
+      next: null,
+    };
 
-var util = require('util');
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      node.next = node;
+      node.prev = node;
+    } else {
+      node.next = this.head;
+      node.prev = this.tail;
 
-function CircularlyLinkedList() {
-  CircularlyLinkedList.super_.call(this);
-}
+      const oldHead = this.head;
+      oldHead.prev = node;
 
-util.inherits(CircularlyLinkedList, DoublyLinkedList);
+      this.tail.next = node;
 
-CircularlyLinkedList.prototype.insertFront = function(val) {
-  var node = {
-    value: val,
-    prev: null,
-    next: null
-  };
+      this.head = node;
+    }
 
-  if (!this.head) {
-    this.head = node;
-    this.tail = node;
-    node.next = node;
-    node.prev = node;
-  } else {
-    //CircularlyLinkedList.super_.prototype.insertFront.call(this,val);
-    node.next = this.head;
-    node.prev = this.tail;
-
-    old_head = this.head;
-    old_head.prev = node;
-
-    this.tail.next = node;
-
-    this.head = node;
+    return this;
   }
 
-  return this;
-};
+  /**
+   * Inserts an item at the end of the list linked list
+   *
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
+   *
+   * @param {any} val
+   * @returns this
+   * @memberof CircularlyLinkedList
+   */
+  insertEnd(val) {
+    const node = {
+      value: val,
+      next: null,
+    };
 
-CircularlyLinkedList.prototype.insertEnd = function(val) {
-  var node = {
-    value: val,
-    next: null
-  };
+    if (!this.tail) {
+      this.tail = node;
+      this.head = node;
+      node.next = node;
+      node.prev = node;
+    } else {
+      this.tail.next = node;
+      this.head.prev = node;
 
-  if (!this.tail) {
-    this.tail = node;
-    this.head = node;
-    node.next = node;
-    node.prev = node;
-  } else {
+      node.next = this.head;
+      node.prev = this.tail;
 
-    this.tail.next = node;
-    this.head.prev = node;
+      this.tail = node;
+    }
 
-    node.next = this.head;
-    node.prev = this.tail;
-
-    this.tail = node;
+    return this;
   }
 
-  return this;
-};
 
-CircularlyLinkedList.prototype.removeFront = function() {
+  /**
+   * Remove and item from the back of the linked list
+   *
+   * Time Complexity: O(1)
+   *
+   * @returns this
+   * @memberof LinkedList
+   */
+  removeFront() {
+    if (!this.head) {
+      // do nothing but return for chaining
+    } else {
+      const newHead = this.head.next;
+      newHead.prev = this.head.prev;
+      this.head = newHead;
+    }
 
-  if (!this.head) {
-    // do nothing but return for chaining
-  } else {
-    var new_head = this.head.next;
-    new_head.prev = this.head.prev;
-    this.head = new_head;
+    return this;
   }
 
-  return this;
-};
+  removeEnd() {
+    if (this.tail) {
+      const newTail = this.tail.prev;
+      newTail.next = this.tail.next;
+      this.tail = newTail;
+    }
 
-CircularlyLinkedList.prototype.removeEnd = function() {
-
-  if (this.tail) {
-    var new_tail = this.tail.prev;
-    new_tail.next = this.tail.next;
-    this.tail = new_tail;
+    return this;
   }
 
-  return this;
-};
+  /**
+   * Make an array representation of the linked list
+   * makes testing life easier
+   *
+   * Time Complexity: O(n)
+   * Space Complexity: O(n)
+   *
+   * @returns this
+   * @memberof LinkedList
+   */
+  toArray() {
+    const arr = [];
 
-CircularlyLinkedList.prototype.toArray = function() {
-  var arr = [];
+    if (!this.head) {
+      return arr;
+    }
 
-  if (!this.head) {
+    arr.push(this.head.value);
+
+    let cur = this.head.next;
+
+    while (cur !== this.head) {
+      arr.push(cur.value);
+      cur = cur.next;
+    }
+
     return arr;
   }
+}
 
-  arr.push(this.head.value);
-
-  var cur = this.head.next;
-
-  while (cur != this.head) {
-    arr.push(cur.value);
-    cur = cur.next;
-  }
-
-  return arr;
-};
-
-module.exports = CircularlyLinkedList;
+export default CircularlyLinkedList;
--- javascript/test/data_structures/circularly_linked_list_test.js
+++ javascript/test/data_structures/circularly_linked_list_test.js
@@ -1,24 +1,24 @@
-var assert = require('assert');
-var CircularlyLinkedList = require('../../data_structures/linked_list/circularly_linked_list.js');
+import assert from 'assert';
+import CircularlyLinkedList from '../../data_structures/linked_list/circularly_linked_list';
 
-// never forget objects are only cleared across tests of functions not with each subtest of a function
-describe('Circularly Linked List', function() {
-
-  describe('#insertFront()', function() {
-    var llist = new CircularlyLinkedList();
-    it("inserts into linked list", function() {
+// never forget objects are only cleared across tests of functions
+// not with each subtest of a function
+describe('Circularly Linked List', () => {
+  describe('#insertFront()', () => {
+    const llist = new CircularlyLinkedList();
+    it('inserts into linked list', () => {
       llist.insertFront(1);
       assert.equal(llist.head.value, 1);
     });
 
-    it("inserts subsequent ahead of older value", function() {
+    it('inserts subsequent ahead of older value', () => {
       llist.insertFront(2);
       assert.equal(llist.head.value, 2);
       assert.equal(llist.head.next.value, 1);
       assert.equal(llist.head.next.prev.value, 2);
     });
 
-    it("allows tail to reach head", function() {
+    it('allows tail to reach head', () => {
       assert.equal(llist.head.value, 2);
       assert.equal(llist.tail.value, 1);
 
@@ -30,9 +30,9 @@ describe('Circularly Linked List', function() {
     });
   });
 
-  describe('#toArray()', function() {
-    var llist = new CircularlyLinkedList();
-    it("converts list to an array", function() {
+  describe('#toArray()', () => {
+    const llist = new CircularlyLinkedList();
+    it('converts list to an array', () => {
       llist.insertFront(1).insertFront(2);
 
       // http://stackoverflow.com/questions/13225274/the-difference-between-assert-equal-and-assert-deepequal-in-javascript-testing-w
@@ -40,15 +40,15 @@ describe('Circularly Linked List', function() {
     });
   });
 
-  describe('#insertEnd()', function() {
-    var llist = new CircularlyLinkedList();
+  describe('#insertEnd()', () => {
+    const llist = new CircularlyLinkedList();
 
-    it("inserts into linked list", function() {
+    it('inserts into linked list', () => {
       llist.insertEnd(1);
       assert.equal(llist.head.value, 1);
     });
 
-    it("inserts subsequent value behind older value", function() {
+    it('inserts subsequent value behind older value', () => {
       llist.insertEnd(2);
       assert.equal(llist.head.value, 1);
       assert.equal(llist.tail.value, 2);
@@ -58,11 +58,11 @@ describe('Circularly Linked List', function() {
       assert.equal(llist.tail.prev.value, 1);
     });
 
-    it("correctly converts toArray", function() {
+    it('correctly converts toArray', () => {
       assert.deepEqual(llist.toArray(), [1, 2]);
     });
 
-    it("allows tail to reach head", function() {
+    it('allows tail to reach head', () => {
       assert.equal(llist.head.value, 1);
       assert.equal(llist.tail.value, 2);
 
@@ -74,17 +74,16 @@ describe('Circularly Linked List', function() {
     });
   });
 
-  describe('#removeFront()', function() {
-
-    it("removes most recently inserted value", function() {
-      var llist = new CircularlyLinkedList();
+  describe('#removeFront()', () => {
+    it('removes most recently inserted value', () => {
+      const llist = new CircularlyLinkedList();
       llist.insertFront(1).insertFront(2);
       llist.removeFront();
       assert.equal(llist.head.value, 1);
     });
 
-    it("doesn't break circularity", function() {
-      var llist = new CircularlyLinkedList();
+    it("doesn't break circularity", () => {
+      const llist = new CircularlyLinkedList();
       llist.insertFront(1).insertFront(2).insertFront(3);
 
       llist.removeFront();
@@ -97,21 +96,19 @@ describe('Circularly Linked List', function() {
 
       assert.equal(llist.tail.prev.value, 2);
       assert.equal(llist.tail.prev.next.value, 1);
-
     });
   });
 
-  describe('#removeEnd()', function() {
-
-    it("removes least recently inserted value", function() {
-      var llist = new CircularlyLinkedList();
+  describe('#removeEnd()', () => {
+    it('removes least recently inserted value', () => {
+      const llist = new CircularlyLinkedList();
       llist.insertFront(1).insertFront(2);
       llist.removeEnd();
       assert.equal(llist.head.value, 2);
     });
 
-    it("doesn't break circularity", function() {
-      var llist = new CircularlyLinkedList();
+    it("doesn't break circularity", () => {
+      const llist = new CircularlyLinkedList();
       llist.insertEnd(1).insertEnd(2).insertEnd(3);
 
       llist.removeEnd();
@@ -125,7 +122,5 @@ describe('Circularly Linked List', function() {
       assert.equal(llist.tail.prev.next.value, 2);
     });
   });
-
-
 });
 
