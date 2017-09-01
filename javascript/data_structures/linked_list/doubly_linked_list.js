3232298c49077f6949dcf348f173f7d01d0956cb
William Harris <wharris@upscalews.com>
2017-09-01 00:47:28 -0700

    es6ify doubly_linked_list

--- javascript/data_structures/linked_list/doubly_linked_list.js
+++ javascript/data_structures/linked_list/doubly_linked_list.js
@@ -1,102 +1,162 @@
-// http://en.wikipedia.org/wiki/Doubly_linked_list
-function DoublyLinkedList() {
-  this.head = null;
-  this.tail = null;
-}
-
-DoublyLinkedList.prototype.insertFront = function(val) {
-  var node = {
-    value: val,
-    prev: null,
-    next: null
-  };
-
-  if (!this.head) {
-    // create special case of one node doubly linked list 
-    this.head = node;
-    this.tail = node;
-  } else {
-    node.next = this.head;
-    this.head.prev = node;
-    this.head = node;
+/**
+ * Implements a doubly linked list
+ *
+ * Note about this implementation that took me ages to figure out when I wrote this
+ *
+ * This version uses a trick for when there is only one node in the list.
+ * Its makes said node both the head and the tail.
+ *
+ * Saves you a large amount of null checking in its methods. Pretty clever
+ *
+ * http://en.wikipedia.org/wiki/Doubly_linked_list
+ * @class DoublyLinkedList
+ */
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  return this;
-};
+  /**
+   * Inserts an item at the front of the list linked list
+   *
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
+   *
+   * @param {any} val
+   * @returns this
+   * @memberof DoublyLinkedList
+   */
+  insertFront(val) {
+    const node = {
+      value: val,
+      prev: null,
+      next: null,
+    };
 
-DoublyLinkedList.prototype.insertEnd = function(val) {
-  var node = {
-    value: val,
-    next: null
-  };
-
-  if (!this.tail) {
-    // create special case of one node doubly linked list 
-    this.tail = node;
-    this.head = node;
-  } else {
-    node.prev = this.tail;
-    this.tail.next = node;
-    this.tail = node;
-  }
-
-  return this;
-};
-
-DoublyLinkedList.prototype.removeFront = function() {
-
-  if (this.head) {
-
-    // special case for removal of node from a list with only one node where 
-    // that node is both the head and the tail
-    //
-    // this is a convention people use to make the code for adding subsequent nodes easier
-    // because you never have to do any null checking
-    if (this.head == this.tail) {
-      this.tail = null;
+    if (!this.head) {
+      // create special case of one node doubly linked list
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
     }
 
-    var new_head = this.head.next;
-    this.head = new_head;
+    return this;
   }
 
-  return this;
-};
+  /**
+   * Inserts an item at the end of the list linked list
+   *
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
+   *
+   * Note that here this function is O(1) unlike in its singularly linked cousin
+   * obviously because we have a link to the final node
+   *
+   * @param {any} val
+   * @returns this
+   * @memberof DoublyLinkedList
+   */
+  insertEnd(val) {
+    const node = {
+      value: val,
+      next: null,
+    };
 
-DoublyLinkedList.prototype.removeEnd = function() {
+    if (!this.tail) {
+      // create special case of one node doubly linked list
+      this.tail = node;
+      this.head = node;
+    } else {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
 
-  if (this.tail) {
+    return this;
+  }
+
+  /**
+   * Remove an item from the front of the linked list
+   *
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
+   *
+   * @returns this
+   * @memberof DoublyLinkedList
+   */
+  removeFront() {
+    if (this.head) {
+      // special case for removal of node from a list with only one node where
+      // that node is both the head and the tail
+      //
+      // this is a convention people use to make the code for adding subsequent nodes easier
+      // because you never have to do any null checking
+      if (this.head === this.tail) {
+        this.tail = null;
+      }
+
+      const newHead = this.head.next;
+      this.head = newHead;
+    }
+
+    return this;
+  }
+
+  /**
+   * Remove and item from the back of the linked list
+   *
+   * Time Complexity: O(1)
+   *
+   * @returns this
+   * @memberof LinkedList
+   */
+  removeEnd() {
+    if (this.tail) {
+      // special case for list with one item
+      if (this.head === this.tail) {
+        this.head = null;
+      }
+
+      const newTail = this.tail.prev;
+      this.tail = newTail;
+      // since this is now no reference to the old head the garbage collector will delete it.
+      // we could do that on our own if we wanted though
+    }
+
+    return this;
+  }
+
+  /**
+   * Make an array representation of the linked list
+   * makes testing life easier
+   *
+   * Time Complexity: O(n)
+   * Space Complexity: O(n)
+   *
+   * @returns this
+   * @memberof LinkedList
+   */
+  toArray() {
+    const arr = [];
 
     // special case for list with one item
-    if (this.head == this.tail) {
-      this.head = null;
+    if (this.head === this.tail) {
+      return [this.head.value];
     }
 
-    var new_tail = this.tail.prev;
-    this.tail = new_tail;
-  // since this is now no reference to the old head the garbage collector will delete it. 
-  // we could do that on our own if we wanted though
+    let cur = this.head;
+
+    while (cur) {
+      arr.push(cur.value);
+      cur = cur.next;
+    }
+
+    return arr;
   }
+}
 
-  return this;
-};
-
-DoublyLinkedList.prototype.toArray = function() {
-  var arr = [];
-
-  // special case for list with one item
-  if (this.head == this.tail) {
-    return [this.head.value];
-  }
-
-  var cur = this.head;
-
-  while (cur) {
-    arr.push(cur.value);
-    cur = cur.next;
-  }
-
-  return arr;
-};
-
-module.exports = DoublyLinkedList;
+export default DoublyLinkedList;
--- javascript/test/data_structures/doubly_linked_list_test.js
+++ javascript/test/data_structures/doubly_linked_list_test.js
@@ -1,16 +1,15 @@
-var assert = require('assert');
-var DoublyLinkedList = require('../../data_structures/linked_list/doubly_linked_list.js');
+import assert from 'assert';
+import DoublyLinkedList from '../../data_structures/linked_list/doubly_linked_list';
 
-describe('Doubly Linked List', function() {
-
-  describe('#insertFront()', function() {
-    var llist = new DoublyLinkedList();
-    it("inserts into linked list", function() {
+describe('Doubly Linked List', () => {
+  describe('#insertFront()', () => {
+    const llist = new DoublyLinkedList();
+    it('inserts into linked list', () => {
       llist.insertFront(1);
       assert.equal(llist.head.value, 1);
     });
 
-    it("inserts subsequent ahead of older value", function() {
+    it('inserts subsequent ahead of older value', () => {
       llist.insertFront(2);
       assert.equal(llist.head.value, 2);
       assert.equal(llist.head.next.value, 1);
@@ -18,16 +17,16 @@ describe('Doubly Linked List', function() {
     });
   });
 
-  describe('#toArray()', function() {
-    it("converts list with one item an array", function() {
-      var llist = new DoublyLinkedList();
+  describe('#toArray()', () => {
+    it('converts list with one item an array', () => {
+      const llist = new DoublyLinkedList();
       llist.insertFront(1);
 
       // http://stackoverflow.com/questions/13225274/the-difference-between-assert-equal-and-assert-deepequal-in-javascript-testing-w
       assert.deepEqual(llist.toArray(), [1]);
     });
-    it("converts list to an array", function() {
-      var llist = new DoublyLinkedList();
+    it('converts list to an array', () => {
+      const llist = new DoublyLinkedList();
       llist.insertFront(1).insertFront(2);
 
       // http://stackoverflow.com/questions/13225274/the-difference-between-assert-equal-and-assert-deepequal-in-javascript-testing-w
@@ -35,15 +34,15 @@ describe('Doubly Linked List', function() {
     });
   });
 
-  describe('#insertEnd()', function() {
-    var llist = new DoublyLinkedList();
+  describe('#insertEnd()', () => {
+    const llist = new DoublyLinkedList();
 
-    it("inserts into linked list", function() {
+    it('inserts into linked list', () => {
       llist.insertEnd(1);
       assert.equal(llist.head.value, 1);
     });
 
-    it("inserts subsequent value behind older value", function() {
+    it('inserts subsequent value behind older value', () => {
       llist.insertEnd(2);
       assert.equal(llist.head.value, 1);
       assert.equal(llist.tail.value, 2);
@@ -53,41 +52,38 @@ describe('Doubly Linked List', function() {
       assert.equal(llist.tail.prev.value, 1);
     });
 
-    it("correctly converts toArray", function() {
+    it('correctly converts toArray', () => {
       assert.deepEqual(llist.toArray(), [1, 2]);
     });
   });
 
-  describe('#removeFront()', function() {
-    var llist = new DoublyLinkedList();
+  describe('#removeFront()', () => {
+    const llist = new DoublyLinkedList();
     llist.insertFront(1);
 
-    it("works corectly with 1 value", function() {
+    it('works corectly with 1 value', () => {
       llist.removeFront();
     });
 
     llist.insertFront(1).insertFront(2);
-    it("removes most recently inserted value", function() {
+    it('removes most recently inserted value', () => {
       llist.removeFront();
       assert.equal(llist.head.value, 1);
     });
   });
 
-  describe('#removeEnd()', function() {
-    var llist = new DoublyLinkedList();
+  describe('#removeEnd()', () => {
+    const llist = new DoublyLinkedList();
     llist.insertFront(1);
 
-    it("works corectly with 1 value", function() {
+    it('works corectly with 1 value', () => {
       llist.removeEnd();
     });
 
     llist.insertFront(1).insertFront(2);
-    it("removes least recently inserted value", function() {
+    it('removes least recently inserted value', () => {
       llist.removeEnd();
       assert.equal(llist.head.value, 2);
     });
   });
-
-
 });
-
