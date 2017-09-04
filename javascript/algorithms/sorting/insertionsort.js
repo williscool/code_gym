3836478b4ed3eeb359d7c9ca1413c55bd650b4c0
William Harris <wharris@upscalews.com>
2017-09-04 02:57:50 -0700

    es6ify insertion sort

--- javascript/algorithms/sorting/insertionsort.js
+++ javascript/algorithms/sorting/insertionsort.js
@@ -1,60 +1,99 @@
-// http://en.wikipedia.org/wiki/Insertion_sort
-var dsalgo = require('../../utilities.js').default;
-var swap = dsalgo.utils.swap;
+/* eslint-disable no-param-reassign */
+// I mean this is litterally a sorting function for an array
+import dsalgo from '../../utilities';
 
-var naive = function(list) {
+const swap = dsalgo.utils.swap;
 
-  var len, i, j;
+/**
+ * Implements Insertion Sort
+ *
+ * Actually a fairly widely used algorithm for small arrays because its:
+ *
+ * - Efficient for (quite) small data sets
+ * - More efficient in practice than most other simple quadratic (i.e., O(n^2)) algorithms such as selection sort or bubble sort
+ * - Stable; i.e., does not change the relative order of elements with equal keys
+ * - In-place; i.e., only requires a constant amount O(1) of additional memory space
+ * - Online; i.e., can sort a list as it receives it
+ * - Performs well on list/araays that are already mostly sorted
+ *
+ * As a matter of fact fun fact v8 the javascript engine powering node.js uses it
+ *
+ * https://github.com/v8/v8/blob/6.3.4/src/js/array.js#L720
+ * https://github.com/v8/v8/blob/6.3.4/src/js/array.js#L734
+ *
+ * http://en.wikipedia.org/wiki/Insertion_sort
+ * @module InsertionSort
+ */
+export default {
 
-  len = list.length;
+  /**
+   * The original version
+   *
+   * @param {array} list
+   * @returns {array}
+   */
+  naive(list) {
+    const len = list.length;
+    let i;
+    let j;
 
-  // for each value in the list starting with the second
-  for (i = 1; i < len; i++) {
-    j = i;
+    // for each value in the list starting with the second
+    for (i = 1; i < len; i += 1) {
+      j = i;
 
-    // while this value j is greater than zero AND value at list[j] is lower than left previous value
-    //
-    // j greater than zero makes sure to not go out of bounds to the left when looking to move the value back
-    //
-    // greater than previous value keeps the while going until we find a value that list[j] is greater than
-    //
-    while (j > 0 && list[j - 1] > list[j]) {
-      list = swap(list, j, j - 1);
-      // --j != j--
-      // http://en.wikipedia.org/wiki/Increment_and_decrement_operators
-      // https://www.youtube.com/watch?v=VbvNp6AjwM8
+      // while this value j is greater than zero AND value at list[j] is lower than left previous value
       //
-      // j = j - 1;
-      --j;
-    }
-  }
-
-  return list;
-};
-
-var optimized = function(list) {
-
-  var len, i, j, value_being_sorted_now;
-
-  len = list.length;
-
-  for (i = 1; i < len; i++) {
-    // store this val to swap into its new place only after we are done moving higher values in front of it
-    value_being_sorted_now = list[i];
-    j = i;
-
-    while (j > 0 && list[j - 1] > value_being_sorted_now) {
-      list[j] = list[j - 1];
-      j = j - 1;
+      // j greater than zero makes sure to not go out of bounds to the left when looking to move the value back
+      //
+      // greater than previous value keeps the while going until we find a value that list[j] is greater than
+      //
+      while (j > 0 && list[j - 1] > list[j]) {
+        list = swap(list, j, j - 1);
+        // --j != j--
+        // http://en.wikipedia.org/wiki/Increment_and_decrement_operators
+        // https://www.youtube.com/watch?v=VbvNp6AjwM8
+        //
+        // comment above left for comedic purposes
+        //
+        // rip to the old call to --j cuz airbnb style.
+        // ironically literally to avoid this very bug
+        //
+        // j = j - 1;
+        j -= 1;
+      }
     }
 
-    list[j] = value_being_sorted_now;
-  }
+    return list;
+  },
+  /**
+   * Optimized Version
+   *
+   * slight optimization by storing the value to swap into place
+   * when we are finished moving the higher values in front of it
+   *
+   * @param {any} list
+   * @returns {array}
+   */
+  optimized(list) {
+    const len = list.length;
+    let i;
+    let j;
+    let valueBeingSortedNow;
 
-  return list;
-};
+    for (i = 1; i < len; i += 1) {
+      // store this val to swap into its new place only after we are done moving higher values in front of it
+      valueBeingSortedNow = list[i];
+      j = i;
 
-module.exports = {
-  naive: naive,
-  optimized: optimized
+      while (j > 0 && list[j - 1] > valueBeingSortedNow) {
+        list[j] = list[j - 1];
+        j -= 1;
+      }
+
+      list[j] = valueBeingSortedNow;
+    }
+
+    return list;
+  },
+
 };
--- javascript/test/algorithms/sorting/insertionsort_test.js
+++ javascript/test/algorithms/sorting/insertionsort_test.js
@@ -1,45 +1,41 @@
-var assert = require('assert');
-var InsertionSort = require('../../../algorithms/sorting/insertionsort.js');
-var dsalgo = require('../../../utilities.js').default;
+import assert from 'assert';
+import InsertionSort from '../../../algorithms/sorting/insertionsort';
+import dsalgo from '../../../utilities';
 
-Object.keys(InsertionSort).forEach(function(key) {
+Object.keys(InsertionSort).forEach((key) => {
+  const fn = InsertionSort[key];
+  const insertionsort = fn;
 
-  var fn = InsertionSort[key];
-  var insertionsort = fn;
-
-  describe(key + ' Insertion Sort', function() {
-
-    describe('should work with arrays', function() {
-      it('that are empty', function() {
+  describe(`${key} Insertion Sort`, () => {
+    describe('should work with arrays', () => {
+      it('that are empty', () => {
         assert.deepEqual(insertionsort([]), []);
       });
 
-      it('with no duplicates', function() {
+      it('with no duplicates', () => {
         assert.deepEqual(insertionsort([9, 2, 8, 6, 1, 3]), [1, 2, 3, 6, 8, 9]);
         assert.deepEqual(insertionsort([5, 2, 2, 6, 1, 3]), [1, 2, 2, 3, 5, 6]);
         assert.deepEqual(insertionsort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
       });
 
-      it('that have duplicates', function() {
+      it('that have duplicates', () => {
         assert.deepEqual(insertionsort([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]), [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
       });
 
-      it('that have negative values', function() {
+      it('that have negative values', () => {
         assert.deepEqual(insertionsort([0, 0, 0, 0, 0, -1]), [-1, 0, 0, 0, 0, 0]);
       });
 
-      it('that are random and non-sorted', function() {
-        var array = dsalgo.utils.makeRandomArray({
-          precision: 0
+      it('that are random and non-sorted', () => {
+        const array = dsalgo.utils.makeRandomArray({
+          precision: 0,
         });
 
         insertionsort(array);
-        for (var i = 1; i < array.length; i++) {
+        for (let i = 1; i < array.length; i += 1) {
           assert((array[i - 1] <= array[i]) === true);
         }
       });
     });
-
   });
-
 });
