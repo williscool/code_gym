e61af39abc6f88f8af3752d4905b5921fb9554af
William Harris <wharris@upscalews.com>
2017-09-03 20:44:14 -0700

    es6ify bubblesort

--- javascript/algorithms/sorting/bubblesort.js
+++ javascript/algorithms/sorting/bubblesort.js
@@ -1,88 +1,117 @@
-// http://en.wikipedia.org/wiki/Bubble_sort#Implementation
-// https://www.youtube.com/watch?v=hR-NXv5Tma0
-// bubble bubble..
+/* eslint-disable no-param-reassign */
+// I mean this is litterally a sorting function for an array
+// its gotta mutate it
+import { dsalgo } from '../../utilities';
 
-var dsalgo = require('../../utilities.js').dsalgo;
-var swap = dsalgo.utils.swap;
+const swap = dsalgo.utils.swap;
 
-var naive = function(list) {
+/**
+ * http://en.wikipedia.org/wiki/Bubble_sort#Implementation
+ *
+ * bubble bubble..
+ * https://www.youtube.com/watch?v=hR-NXv5Tma0
+ *
+ * Exports several types of bubble sorts
+ *
+ * Time Complexity: O(n^2)
+ *
+ * Also this basically the worst sorting algorithm there is on multiple fronts
+ * https://en.wikipedia.org/wiki/Bubble_sort#In_practice
+ *
+ * @module BubbleSort
+ */
+export default {
 
-  var len, sorted, i;
+  /**
+   * This is the orginal version
+   *
+   * @param {array} list
+   * @returns
+   */
+  naive(list) {
+    const len = list.length;
+    let sorted;
+    let i;
 
-  len = list.length;
+    do {
+      // assume array is sorted at first and let the for loop tell us if it isn't
+      sorted = true;
 
-  do {
-    // assume array is sorted at first and let the for loop tell us if it isn't
-    sorted = true;
-
-    for (i = 1; i < len; i++) {
-
-      if (list[i - 1] > list[i]) {
-        list = swap(list, i - 1, i);
-        sorted = false;
+      for (i = 1; i < len; i += 1) {
+        if (list[i - 1] > list[i]) {
+          list = swap(list, i - 1, i);
+          sorted = false;
+        }
       }
-    }
+    } while (!sorted);
 
-  } while (!sorted);
+    return list;
+  },
 
-  return list;
-};
+  /**
+   * This version is optimized by skipping examing the already sort items at each pass
+   *
+   * @param {array} list
+   * @returns
+   */
+  optimized(list) {
+    let len;
+    let sorted;
+    let i;
 
-var optimized = function(list) {
+    len = list.length;
 
-  var len, sorted, i;
+    do {
+      // assume array is sorted at first and let the for loop tell us if it isn't
+      sorted = true;
 
-  len = list.length;
-
-  do {
-    // assume array is sorted at first and let the for loop tell us if it isn't
-    sorted = true;
-
-    for (i = 1; i < len; i++) {
-
-      if (list[i - 1] > list[i]) {
-        list = swap(list, i - 1, i);
-        sorted = false;
+      for (i = 1; i < len; i += 1) {
+        if (list[i - 1] > list[i]) {
+          list = swap(list, i - 1, i);
+          sorted = false;
+        }
       }
-    }
 
-    // now all we need to is decrement the number of elements to look at
-    // the last item on each iteration is already in the correct place
-    //
-    // it.. wait for it... bubbled.. to the top
-    // http://cow.org/csi/
-    len = len - 1;
-  } while (!sorted);
+      // now all we need to is decrement the number of elements to look at
+      // the last item on each iteration is already in the correct place
+      //
+      // it.. wait for it... bubbled.. to the top
+      // http://cow.org/csi/
+      len -= 1;
+    } while (!sorted);
 
-  return list;
-};
+    return list;
+  },
 
-var stopAtLastSorted = function(list) {
-  // this algorithm assumes that the last element we had to swap was the last element out of place at that iteration
-  // so it shortens the amount of elements looked at on subsequent iterations by that many
+  /**
+   * Another Optimized version of Bubblesort
+   *
+   * This algorithm assumes that the last element we had to swap was the last element out of place at that iteration
+   * so it shortens the amount of elements looked at on subsequent iterations by that many
+   *
+   * @param {array} list
+   * @returns
+   */
+  stopAtLastSorted(list) {
+    let len;
+    let i;
+    let newLen;
 
-  var len, sorted, i, newLen;
+    len = list.length;
 
-  len = list.length;
-
-  do {
-    newLen = 0;
-    for (i = 0; i < len; i++) {
-
-      if (list[i - 1] > list[i]) {
-        list = swap(list, i - 1, i);
-        newLen = i;
+    do {
+      newLen = 0;
+      for (i = 0; i < len; i += 1) {
+        if (list[i - 1] > list[i]) {
+          list = swap(list, i - 1, i);
+          newLen = i;
+        }
       }
-    }
 
-    len = newLen;
-  } while (len !== 0);
+      len = newLen;
+    } while (len !== 0);
 
-  return list;
-};
+    return list;
+  },
 
-module.exports = {
-  naive: naive,
-  optimized: optimized,
-  stopAtLastSorted:stopAtLastSorted
 };
--- javascript/test/algorithms/sorting/bubblesort_test.js
+++ javascript/test/algorithms/sorting/bubblesort_test.js
@@ -1,37 +1,39 @@
-var assert = require('assert');
-var BubbleSort = require('../../../algorithms/sorting/bubblesort.js');
-var dsalgo = require('../../../utilities.js').dsalgo;
+import assert from 'assert';
+import BubbleSort from '../../../algorithms/sorting/bubblesort';
+import { dsalgo } from '../../../utilities';
 
-// inspired by: https://github.com/addyosmani/bubblesort/blob/master/test/test.js
-// http://stackoverflow.com/questions/7440001/iterate-over-object-keys-in-node-js
-Object.keys(BubbleSort).forEach(function(key) {
-  var fn = BubbleSort[key];
-  var bubblesort = fn;
+/**
+ * inspired by: https://github.com/addyosmani/bubblesort/blob/master/test/test.js
+ * http://stackoverflow.com/questions/7440001/iterate-over-object-keys-in-node-js
+ */
+Object.keys(BubbleSort).forEach((key) => {
+  const fn = BubbleSort[key];
+  const bubblesort = fn;
 
-  describe(key + ' Bubble Sort', function() {
-
-    it('should work with empty arrays', function() {
+  describe(`${key} Bubble Sort`, () => {
+    it('should work with empty arrays', () => {
       assert.deepEqual(bubblesort([]), []);
     });
 
-    it('should work with ascending arrays', function() {
-      assert.deepEqual(bubblesort([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]), [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
+    it('should work with ascending arrays', () => {
+      assert.deepEqual(
+        bubblesort(
+          [3, 1, 4, 1, 5, 9, 2, 6, 5, 4]),
+        [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
       assert.deepEqual(bubblesort([9, 2, 8, 6, 1, 3]), [1, 2, 3, 6, 8, 9]);
       assert.deepEqual(bubblesort([5, 2, 2, 6, 1, 3]), [1, 2, 2, 3, 5, 6]);
       assert.deepEqual(bubblesort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
       assert.deepEqual(bubblesort([0, 0, 0, 0, 0, -1]), [-1, 0, 0, 0, 0, 0]);
     });
 
-    it('should work with random non-sorted arrays', function() {
-      var array = dsalgo.utils.makeRandomArray({
-        precision: 0
+    it('should work with random non-sorted arrays', () => {
+      const array = dsalgo.utils.makeRandomArray({
+        precision: 0,
       });
       bubblesort(array);
-      for (var i = 1; i < array.length; i++) {
+      for (let i = 1; i < array.length; i += 1) {
         assert((array[i - 1] <= array[i]) === true);
       }
     });
-
   });
-
 });
