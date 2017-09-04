22d1229c1965b4850c47b8a74387faaa99d909c8
William Harris <wharris@upscalews.com>
2017-09-04 14:41:44 -0700

    es6ify quicksort

--- javascript/algorithms/sorting/quicksort.js
+++ javascript/algorithms/sorting/quicksort.js
@@ -1,72 +1,111 @@
-// http://en.wikipedia.org/wiki/Quicksort
-// inplace
-var dsalgo = require('../../utilities.js').default;
-var swap = dsalgo.utils.swap;
+/* eslint-disable no-param-reassign */
+// I mean this is litterally a sorting function for an array
+import dsalgo from '../../utilities';
 
+const swap = dsalgo.utils.swap;
+
+/**
+ * Chooses the pivot value for the partition function
+ *
+ * lol worthy thing that happend with this function
+ *
+ * So you gotta make sure your order of operations is correct
+ * because if this pivot returns an incorrect index everything is fucked
+ *
+ * http://googleresearch.blogspot.com/2006/06/extra-extra-read-all-about-it-nearly.html
+ *
+ * I prefer using the midpoint but some algos call for using the value at the lowest index aka lo
+ * i.e. https://www.hackerrank.com/challenges/quicksort2
+ *
+ * fun fact though that algorithm causes worst case behavior on arrays that are already sorted
+ *
+ * https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot
+ *
+ *
+ * @param {number} lo - lowest index to use to calculate pivot
+ * @param {number} hi - highest index to used to calculate pivot
+ * @returns {number}
+ */
+function choosePivot(hi, lo) {
+  // eslint-disable-next-line no-bitwise
+  return (hi + lo) >> 1;
+}
+
+/**
+ * Partitions the array's values into values that are higher and lower
+ * than the value at a pivot index
+ *
+ * https://www.youtube.com/watch?v=pZ12_E5R3qc
+ *
+ * @param {array} list - input array to sort
+ * @param {number} lo - lowest index to use to calculate pivot
+ * @param {number} hi - highest index to used to calculate pivot
+ * @returns {array}
+ */
+function partition(list, lo, hi) {
+  // pick our pivot value
+  const pivotIndex = choosePivot(lo, hi);
+  const pivotVal = list[pivotIndex];
+
+  // move the value of the pivot to the highest index so that it is out of the way
+  // while we compare all of the other element's values to it to figure out where to move it to
+  list = swap(list, pivotIndex, hi);
+  let indexToMoveValueTo = lo;
+
+  // once this for loop completes every value before indexToMoveValueTo will be lower than pivotVal and everyone after will be higher
+  for (let i = lo; i < hi; i += 1) {
+    if (list[i] < pivotVal) {
+      list = swap(list, i, indexToMoveValueTo);
+      indexToMoveValueTo += 1;
+    }
+  }
+
+  // now that the other elements are in their new positions we can move the pivot value to its final place.
+  list = swap(list, indexToMoveValueTo, hi);
+
+  // then we return the value of the index we just put the pivot in
+  // that is our new pivot index
+  return indexToMoveValueTo;
+}
+
+/**
+ * Quicksort
+ *
+ * This is the inplace (it sorts the input array as a opposed to returning a sorted copy)
+ *
+ * One of the most commonly used very performant (in the avg case on large array) sorting algorithms
+ *
+ * Average Case Time Complexity: O(n log(n))
+ * Worse Case Time Complexity: O(n^2)
+ *
+ * As a matter of fact it is used in v8 (the javascript engine powering node.js) for arrays
+ * https://github.com/v8/v8/blob/6.3.4/src/js/array.js#L768
+ *
+ * http://en.wikipedia.org/wiki/Quicksort
+ *
+ * @param {array} list - input array to sort
+ * @param {number} lo - lowest index to use to calculate pivot
+ * @param {number} hi - highest index to used to calculate pivot
+ * @returns {array}
+ */
 function quicksort(list, lo, hi) {
-
   if (lo < hi) {
-    var index_of_pivot = partition(list, lo, hi);
-    quicksort(list, lo, index_of_pivot - 1);
-    quicksort(list, index_of_pivot + 1, hi);
+    const indexOfPivot = partition(list, lo, hi);
+    quicksort(list, lo, indexOfPivot - 1);
+    quicksort(list, indexOfPivot + 1, hi);
   }
 
   return list;
 }
 
-// https://www.youtube.com/watch?v=pZ12_E5R3qc
-function partition(list, lo, hi) {
-
-  var pivotIndex, pivotVal, i, index_to_move_value_to;
-
-  // pick our pivot value
-  pivotIndex = choosePivot(lo, hi);
-  pivotVal = list[pivotIndex];
-
-  // move the value of the pivot to the highest index so that it is out of the way
-  // while we compare all of the other element's values to it to figure out where to move it to
-
-  list = swap(list, pivotIndex, hi);
-  index_to_move_value_to = lo;
-
-  //once this for loop completes every value before index_to_move_value_to will be lower than pivotVal and everyone after will be higher
-  for (i = lo; i < hi; i++) {
-
-    if (list[i] < pivotVal) {
-      list = swap(list, i, index_to_move_value_to);
-      index_to_move_value_to = index_to_move_value_to + 1;
-    }
-  }
-
-  // now that the other elements are in their new positions we can move the pivot value to its final place.
-  list = swap(list, index_to_move_value_to, hi);
-
-  // then we return the value of the index we just put the pivot in
-  // that is our new pivot index
-  return index_to_move_value_to;
-}
-
-function choosePivot(hi, lo) {
-  // lol worthy thing that happend with this function
-  //
-  // so you gotta make sure your order of operations is correct
-  // because if this pivot returns and incorrect index everything is fucked
-  //
-  // http://googleresearch.blogspot.com/2006/06/extra-extra-read-all-about-it-nearly.html
-  //
-  // I prefer using the midpoint but some algos call for using the first aka lo
-  // i.e. https://www.hackerrank.com/challenges/quicksort2
-  //
-  // https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot
-  //
-  return (hi + lo) >> 1;
-}
-
-module.exports = function(list) {
+/**
+ * Module that exports quicksort function with its common invokation
+ * (with the input list, the first index of the input, and the last index)
+ *
+ * @module quicksort
+ * @exports {{quicksort}}
+ * @param {any} list
+ */
+export default function (list) {
   return quicksort(list, 0, list.length - 1);
-};
-//
-// super awesome short quick sort
-//
-// http://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Quicksort#JavaScript
-// But I guess its out of place because it creates new arrays and then adds them all back together at the end
+}
--- javascript/test/algorithms/sorting/quicksort_test.js
+++ javascript/test/algorithms/sorting/quicksort_test.js
@@ -1,42 +1,40 @@
-var assert = require('assert');
-var quicksort = require('../../../algorithms/sorting/quicksort.js');
-var dsalgo = require('../../../utilities.js').default;
+import assert from 'assert';
+import dsalgo from '../../../utilities';
+import quicksort from '../../../algorithms/sorting/quicksort';
 
-describe('Quick Sort', function() {
-
-  describe('should work with arrays', function() {
-    it('that are empty', function() {
+describe('Quick Sort', () => {
+  describe('should work with arrays', () => {
+    it('that are empty', () => {
       assert.deepEqual(quicksort([]), []);
     });
 
-    it('that are have 1 element', function() {
+    it('that are have 1 element', () => {
       assert.deepEqual(quicksort([1]), [1]);
     });
 
-    it('with no duplicates', function() {
+    it('with no duplicates', () => {
       assert.deepEqual(quicksort([9, 2, 8, 6, 1, 3]), [1, 2, 3, 6, 8, 9]);
       assert.deepEqual(quicksort([5, 2, 2, 6, 1, 3]), [1, 2, 2, 3, 5, 6]);
       assert.deepEqual(quicksort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
     });
 
-    it('that have duplicates', function() {
+    it('that have duplicates', () => {
       assert.deepEqual(quicksort([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]), [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
     });
 
-    it('that have negative values', function() {
+    it('that have negative values', () => {
       assert.deepEqual(quicksort([0, 0, 0, 0, 0, -1]), [-1, 0, 0, 0, 0, 0]);
     });
 
-    it('that are random and non-sorted', function() {
-      var array = dsalgo.utils.makeRandomArray({
-        precision: 0
+    it('that are random and non-sorted', () => {
+      const array = dsalgo.utils.makeRandomArray({
+        precision: 0,
       });
 
       quicksort(array);
-      for (var i = 1; i < array.length; i++) {
+      for (let i = 1; i < array.length; i += 1) {
         assert((array[i - 1] <= array[i]) === true);
       }
     });
   });
-
 });
