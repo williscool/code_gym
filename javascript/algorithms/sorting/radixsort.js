48fc3c532c634a6b4d5c42bd3950c8d304879fba
William Harris <wharris@upscalews.com>
2017-09-04 19:37:47 -0700

    es6ify radix sort

--- javascript/algorithms/sorting/radixsort.js
+++ javascript/algorithms/sorting/radixsort.js
@@ -1,72 +1,111 @@
-/*jshint loopfunc:true */
-//http://stackoverflow.com/questions/10451865/jshint-wont-let-me-use-foreach-in-a-for-loop
-//
-// http://en.wikipedia.org/wiki/Radix_sort#Least_significant_digit_radix_sorts
-//
-// inspired by
-// http://www.geeksforgeeks.org/radix-sort/
-// https://github.com/kennyledet/Algorithm-Implementations/blob/master/Radix_Sort/Java/PatrickYevsukov/radix_sort.java
-// http://www.dreamincode.net/forums/topic/199650-least-significant-digit-radix-sort/page__view__findpost__p__1166098
-//
-// https://medium.com/@tyguyo/all-sorts-of-sorts-5da9873aa046
-// https://codehost.wordpress.com/2011/07/22/radix-sort/
-//
-// only works with positve numbers
+/* eslint-disable no-loop-func, no-param-reassign  */
+// again this is litterally a sort function for arrays
+// also i'll make as many forEach functions in a look as I damn well please lol
+// http://stackoverflow.com/questions/10451865/jshint-wont-let-me-use-foreach-in-a-for-loop
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+/**
+ * returns the maximum value in a number array
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+ *
+ * @param {number[]} numArray
+ * @returns
+ */
 function arrayMax(numArray) {
   return Math.max.apply(null, numArray);
 }
 
-module.exports = function(list) {
+/**
+ * Varying Base Log in js
+ *
+ * http://stackoverflow.com/a/8492462/511710
+ *
+ * @param {any} b
+ * @param {any} n
+ * @returns
+ */
+function log(b, n) {
+  return Math.log(n) / Math.log(b);
+}
 
+/**
+ * Get the digit at a place in the number
+ *
+ * Used to chooze the bucket to place a number in for the radix sort
+ *
+ * @param {any} base
+ * @param {any} value
+ * @param {any} digit
+ * @returns
+ */
+function indexFunction(base, value, digit) {
+  return Math.floor((value / (base ** digit)) % base);
+}
+
+/**
+ * Radix sort
+ *
+ * This, like counting sort, is another form of bucket sort
+ *
+ * In this the buckets are decided by the digits in each place of a number based on the base
+ *
+ * http://en.wikipedia.org/wiki/Radix_sort#Least_significant_digit_radix_sorts
+ *
+ * inspired by
+ * http://www.geeksforgeeks.org/radix-sort/
+ * https://github.com/kennyledet/Algorithm-Implementations/blob/master/Radix_Sort/Java/PatrickYevsukov/radix_sort.java
+ * http://www.dreamincode.net/forums/topic/199650-least-significant-digit-radix-sort/page__view__findpost__p__1166098
+ *
+ * https://medium.com/@tyguyo/all-sorts-of-sorts-5da9873aa046
+ * https://codehost.wordpress.com/2011/07/22/radix-sort/
+ *
+ * only works with positve numbers
+ *
+ * @module RadixSort
+ * @export RadixSort
+ * @param {any} list
+ * @param {number} base - the base of the numbers we are sorting. defaults to 10 aka decimal
+ * @returns
+ */
+export default function (list, base = 10) {
   if (list.length < 2) {
     return list;
   }
+  const buckets = [];
 
-  var max, maxLen, i,
-    base = 10,
-    buckets = [], digit;
-
-  // http://stackoverflow.com/a/8492462/511710
-  var log = function(b, n) {
-    return Math.log(n) / Math.log(b);
-  };
-
-  var index_function = function(value, digit) {
-    // get the digit at a place in the number
-    return Math.floor(value / Math.pow(base, digit) % base);
-  };
-
-  max = arrayMax(list);
-  maxLen = Math.floor(log(base, max) + 1);
-
-  i = 0;
+  // make buckets for numbers to go into
+  // another bucket sort
+  let i = 0;
   while (i < base) {
-    // make buckets for numbers to go into
-    // another bucket sort
     buckets[i] = [];
-    i++;
+    i += 1;
   }
 
-  for (digit = 0; digit < maxLen; digit++) {
+  const max = arrayMax(list);
+  const maxLen = Math.floor(log(base, max) + 1);
 
-    // im sorry im sick of writing for loops
-    list.forEach(function(val) {
-      //var index = index_function(val,digit); 
-      //if(!buckets[index]) debugger;
-      buckets[index_function(val, digit)].push(val);
+  // now we will place each number into a bucket
+  // based on each digit at a place in it starting with the least siginificat
+  for (let digit = 0; digit < maxLen; digit += 1) {
+    // place numbers in to bucket based on there significant digit
+    // i.e. the 0 in the number 10 in a first pass or the 1 in a second
+
+    // Got tired of writing for loops
+    list.forEach((val) => {
+      // let index = indexFunction(val,digit);
+      // if(!buckets[index]) debugger;
+      buckets[indexFunction(base, val, digit)].push(val);
     });
 
-    // now that we've built the buckets and shifted them 
+    // now that we've built the buckets and shifted them
     // we need to flatten them back into the list
     list = [];
-    buckets.forEach(function(val, i) {
+    buckets.forEach((val, index) => {
       list = list.concat(val);
-      // then reset buckets to get them ready for next round
-      buckets[i] = [];
+      // then reset buckets to get them ready for next round at the next place
+      buckets[index] = [];
     });
   }
 
   return list;
-};
+}
--- javascript/test/algorithms/sorting/radixsort_test.js
+++ javascript/test/algorithms/sorting/radixsort_test.js
@@ -1,21 +1,20 @@
-var assert = require('assert');
-var radixsort = require('../../../algorithms/sorting/radixsort.js');
-var dsalgo = require('../../../utilities.js').default;
+import assert from 'assert';
+import radixsort from '../../../algorithms/sorting/radixsort';
+import dsalgo from '../../../utilities';
 
-describe('Radix Sort', function() {
-
-  describe('should work with arrays', function() {
-    it('that are empty', function() {
+describe('Radix Sort', () => {
+  describe('should work with arrays', () => {
+    it('that are empty', () => {
       assert.deepEqual(radixsort([]), []);
     });
 
-    it('with no duplicates', function() {
+    it('with no duplicates', () => {
       assert.deepEqual(radixsort([9, 2, 8, 6, 1, 3]), [1, 2, 3, 6, 8, 9]);
       assert.deepEqual(radixsort([5, 2, 2, 6, 1, 3]), [1, 2, 2, 3, 5, 6]);
       assert.deepEqual(radixsort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
     });
 
-    it('that have duplicates', function() {
+    it('that have duplicates', () => {
       assert.deepEqual(radixsort([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]), [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
     });
 
@@ -23,17 +22,16 @@ describe('Radix Sort', function() {
     // you could drop a numbers sign sort them in a seperate array and reverse it
     // it in the end if you needed to support negative numbers
 
-    it('that are random and non-sorted', function() {
-      var array = dsalgo.utils.makeRandomArray({
-        precision: 0
+    it('that are random and non-sorted', () => {
+      let array = dsalgo.utils.makeRandomArray({
+        precision: 0,
       });
 
       array = radixsort(array);
 
-      for (var i = 1; i < array.length; i++) {
+      for (let i = 1; i < array.length; i += 1) {
         assert((array[i - 1] <= array[i]) === true);
       }
     });
   });
-
 });
