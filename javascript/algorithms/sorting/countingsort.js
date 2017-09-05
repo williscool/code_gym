d4cd11da18d677eb05485212b71a1de3a6b22f3c
William Harris <wharris@upscalews.com>
2017-09-04 18:12:38 -0700

    es6ify counting sort

--- javascript/algorithms/sorting/countingsort.js
+++ javascript/algorithms/sorting/countingsort.js
@@ -1,36 +1,39 @@
-// http://en.wikipedia.org/wiki/Counting_sort
-// inspired by http://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Counting_sort
-//
-//
-// turns out bucket sort is basically just a generalization of counting sort
-// http://en.wikipedia.org/wiki/Bucket_sort#Comparison_with_other_sorting_algorithms
-//
-// but instead of a bucket for every number you can make up your own arbitrariliy sized buckets
-//
-// presumably / preferably based off of some knowledge that would help you make the buckets some uniform size
-// then you either recursively bbecket sort or use another sort perhaps selection sort on the smaller buckets
-//
-//  http://en.wikipedia.org/wiki/Bucket_sort#Optimizations
-//
-//  so no need to re do for that
-//
-//  https://www.interviewcake.com/question/ruby/top-scores
 
-module.exports = function(list, power) {
-
-  var i, j, k, min, max,
-    counts = [],
-    result = [],
-    len = list.length;
-
-  var index_function = function(value, min) {
-    return value - min;
-  };
+/**
+ * Counting Sort
+ *
+ * http://en.wikipedia.org/wiki/Counting_sort
+ *
+ * inspired by http://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Counting_sort
+ *
+ * turns out bucket sort is basically just a generalization of counting sort
+ * http://en.wikipedia.org/wiki/Bucket_sort#Comparison_with_other_sorting_algorithms
+ *
+ * but instead of a bucket for every number you can make up your own arbitrariliy sized buckets
+ *
+ * presumably / preferably based off of some knowledge that would help you make the buckets some uniform size
+ * then you either recursively bbecket sort or use another sort perhaps selection sort on the smaller buckets
+ *
+ * http://en.wikipedia.org/wiki/Bucket_sort#Optimizations
+ *
+ * so no need to re do for that
+ *
+ * https://www.interviewcake.com/question/ruby/top-scores
+ *
+ * @module CountingSort
+ *
+ * @param {array} list
+ * @returns {array}
+ */
+export default function (list) {
+  const counts = [];
+  const result = [];
+  const len = list.length;
 
   // calculate min and max from the numbers in the list
-  min = list[0];
-  max = list[0];
-  list.forEach(function(val) {
+  let min = list[0];
+  let max = list[0];
+  list.forEach((val) => {
     if (val > max) {
       max = val;
     }
@@ -40,36 +43,28 @@ module.exports = function(list, power) {
     }
   });
 
-  if (power) {
-    // overwite the index function to use a certain significant digit
-    index_function = function(value, min) {
-
-      return ((value / power) % 10) - min;
-    };
-  }
-
-  // setup array to hold counts of differnt values
-  for (i = 0; i < max - min + 1; i++) {
+  // setup array to hold counts of different values
+  for (let i = 0; i < (max - min) + 1; i += 1) {
     counts[i] = 0;
   }
 
   // count number of distinct values in their respective array place
-  for (i = 0; i < len; i++) {
+  for (let i = 0; i < len; i += 1) {
     // current value minus the min to keep it in array bounds
-    counts[index_function(list[i],min)]++;
+    counts[list[i] - min] += 1;
   }
 
-  k = 0;
+  let k = 0;
 
   // put output values in result array
   //
   // repeat values based on count at counts index
-  counts.forEach(function(val, i) {
-    for (j = 0; j < counts[i]; j++) {
-      result[k] = i + min;
-      k = k + 1;
+  counts.forEach((val, index) => {
+    for (let j = 0; j < counts[index]; j += 1) {
+      result[k] = index + min;
+      k += 1;
     }
   });
 
   return result;
-};
+}
--- javascript/test/algorithms/sorting/countingsort_test.js
+++ javascript/test/algorithms/sorting/countingsort_test.js
@@ -1,40 +1,38 @@
-var assert = require('assert');
-var countingsort = require('../../../algorithms/sorting/countingsort.js');
-var dsalgo = require('../../../utilities.js').default;
+import assert from 'assert';
+import countingsort from '../../../algorithms/sorting/countingsort';
+import dsalgo from '../../../utilities';
 
-describe('Counting Sort', function() {
-
-  describe('should work with arrays', function() {
-    it('that are empty', function() {
+describe('Counting Sort', () => {
+  describe('should work with arrays', () => {
+    it('that are empty', () => {
       assert.deepEqual(countingsort([]), []);
     });
 
-    it('with no duplicates', function() {
+    it('with no duplicates', () => {
       assert.deepEqual(countingsort([9, 2, 8, 6, 1, 3]), [1, 2, 3, 6, 8, 9]);
       assert.deepEqual(countingsort([5, 2, 2, 6, 1, 3]), [1, 2, 2, 3, 5, 6]);
       assert.deepEqual(countingsort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
     });
 
-    it('that have duplicates', function() {
+    it('that have duplicates', () => {
       assert.deepEqual(countingsort([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]), [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
     });
 
-    it('that have negative values', function() {
+    it('that have negative values', () => {
       assert.deepEqual(countingsort([0, 0, 0, 0, 0, -1]), [-1, 0, 0, 0, 0, 0]);
     });
 
-    it('that are random and non-sorted', function() {
-      var array = dsalgo.utils.makeRandomArray({
-        precision: 0
+    it('that are random and non-sorted', () => {
+      let array = dsalgo.utils.makeRandomArray({
+        precision: 0,
       });
 
       // counting sort creates a new array
       // and thus isnt in place and we need to reassign array to new array
       array = countingsort(array);
-      for (var i = 1; i < array.length; i++) {
+      for (let i = 1; i < array.length; i += 1) {
         assert((array[i - 1] <= array[i]) === true);
       }
     });
   });
-
 });
