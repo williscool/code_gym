0edf8a6b22b832eb4a736ba69dc8a8f3bcff8be2
William Harris <wharris@upscalews.com>
2017-09-04 19:51:58 -0700

    es6ify seq search

--- javascript/algorithms/searching/sequentialsearch.js
+++ javascript/algorithms/searching/sequentialsearch.js
@@ -1,14 +1,37 @@
-// http://en.wikipedia.org/wiki/Linear_search
-module.exports = function(list, needle) {
-  var len = list.length, i;
+/**
+ * Sequential or Linear Search
+ *
+ * Hands down one of the simplest algorithms there is.
+ *
+ * You scan the array... you find the value or you dont lol.
+ *
+ * No more complicated than that.
+ *
+ * http://en.wikipedia.org/wiki/Linear_search
+ *
+ * Note:
+ *
+ * One day I want to make a troll challenge using this knowledge
+ *
+ * https://en.wikipedia.org/wiki/Linear_search#Application
+ *
+ * All you need is the right size (not too big not) unsorted data
+ *
+ * @module SequentialSearch
+ * @param {number[]} list
+ * @param {number} needle
+ * @returns {boolean}
+ */
+export default function (list, needle) {
+  const len = list.length;
 
-  i = 0;
+  let i = 0;
   while (i < len) {
     // if we find the value return its index
-    if (list[i] == needle) return i;
-    i++;
+    if (list[i] === needle) return i;
+    i += 1;
   }
 
   // if not return false
   return false;
-};
+}
--- javascript/test/algorithms/searching/sequentialsearch_test.js
+++ javascript/test/algorithms/searching/sequentialsearch_test.js
@@ -1,37 +1,35 @@
-var assert = require('assert');
-var seqsearch = require('../../../algorithms/searching/sequentialsearch.js');
-var dsalgo = require('../../../utilities.js').default;
+import assert from 'assert';
+import seqsearch from '../../../algorithms/searching/sequentialsearch';
+import dsalgo from '../../../utilities';
 
-describe('Sequential Search', function() {
-
-  describe('should work with arrays', function() {
-    it('that are empty', function() {
+describe('Sequential Search', () => {
+  describe('should work with arrays', () => {
+    it('that are empty', () => {
       assert.equal(seqsearch([], 1), false);
     });
 
-    it('with no duplicates', function() {
+    it('with no duplicates', () => {
       assert.equal(seqsearch([9, 2, 8, 6, 1, 3], 9), 0);
       assert.equal(seqsearch([5, 2, 2, 6, 1, 3], 7), false);
     });
 
-    it('that have duplicates', function() {
+    it('that have duplicates', () => {
       assert.equal(seqsearch([3, 1, 4, 1, 5, 9, 2, 6, 5, 4], 1), 1);
     });
 
-    it('that are random and non-sorted', function() {
-      var array = dsalgo.utils.makeRandomArray({
-        precision: 0
+    it('that are random and non-sorted', () => {
+      const array = dsalgo.utils.makeRandomArray({
+        precision: 0,
       });
 
-      var value = array[Math.floor(array.length / 2)];
+      const value = array[Math.floor(array.length / 2)];
 
       // we cant be sure there are no dupes since the array is random
-      //  so we cant ask for a specfic index.
+      //  so we cant ask for a specfic index... (because we might get to duplicate value before the index return on line 25)
+      //
       //  but we can make sure that the answer we get is not false
       //
       assert(seqsearch(array, value) !== false);
     });
-
   });
-
 });
