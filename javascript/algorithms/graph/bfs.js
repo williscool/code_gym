404f7121854e171ec0b09a5d89d2d7914da3bff7
William Harris <wharris@upscalews.com>
2017-09-11 21:18:56 -0700

    es6ify and doc breadth first search

--- javascript/algorithms/graph/bfs.js
+++ javascript/algorithms/graph/bfs.js
@@ -1,93 +1,112 @@
-// http://en.wikipedia.org/wiki/Breadth-first_search
+import QueueTypes from '../../data_structures/queue';
 
-// https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/a/breadth-first-search-and-its-uses
-// http://en.wikipedia.org/wiki/Breadth-first_search
-//
-// Great article on asymptotic analysis of this algorithm and graph algos in general
-// https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/a/analysis-of-breadth-first-search
+const Queue = QueueTypes.doubly_linked_list;
 
-/*
+/**
+ * http://en.wikipedia.org/wiki/Breadth-first_search
+ *
+ * https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/a/breadth-first-search-and-its-uses
+ * http://en.wikipedia.org/wiki/Breadth-first_search
+ *
+ * Great article on asymptotic analysis of this algorithm and graph algos in general
+ * https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/a/analysis-of-breadth-first-search
+ *
  * Performs a breadth-first search on a graph
- * @param {Graph} graph - Graph object from which we access an adjancency list
- * @returns {array} Array of objects describing each vertex, like
- *     [{distance: _, predecessor: _ }]
+ *
+ * this and the other graph algorithms probably make more sense as a part of the graph object
+ * then their own seperate clasy things but its a nice way to keep the code seperate
+ * and not make that any bigger
+ *
+ * fun observation level order traversing a binary search tree is a breadth first search
+ * but for breadth first search on a graph a node can be connected to more than 2 other nodes
+ *
+ * Time Complexity O(V + E)
+ *
+ * @class BFS
  */
+class BFS {
+  /**
+   * Creates an instance of Breadth First Search.
+   *
+   * @param {Graph} graph Graph object from which we access an adjancency list
+   * @param {any} startVertex the vertex in the graph to start the traversal from
+   * @param {Function} fn optional early exit condition for the traversal. usually that a goal vertex is reached
+   * @returns {array} Array of objects describing each vertex, like
+   *     [{distance: _, predecessor: _ }]
+   * @memberof BFS
+   */
+  constructor(graph, startVertex, fn) {
+    this.startVertex = startVertex;
+    if (graph.order() < 1) return Error("come on dog there's no nodes in this graph.");
 
-// this and the other graph algorithms probably make more sense as a part of the graph object
-// then their own seperate clasy things but its a nice way to keep the code seperate
-// and not make that any bigger
+    this.info = [];
 
-// fun observation level order traversing a binary search tree is a depth first search
-// but for depth first search on a graph a node can be connected to more than 2 other nodes
-
-var Queue = require('../../data_structures/queue.js').default.doubly_linked_list;
-
-function BFS(graph, start_vertex, fn) {
-  this.start_vertex = start_vertex;
-  if (graph.order() < 1) return Error("come on dog there's no nodes in this graph.");
-
-  this.info = [];
-
-  for (var i = 0; i <= graph.order(); i++) {
-    this.info[i] = {
-      distance: null,
-      predecessor: null
-    };
-  }
-
-  this.info[start_vertex].distance = 0;
-
-  var queue = new Queue();
-  queue.enqueue(start_vertex);
-
-
-  // Traverse the graph
-
-  // As long as the queue is not empty:
-  //  Repeatedly dequeue a vertex v from the queue.
-  //
-  //  For each neighbor w of v that has not been visited:
-  //     Set distance to 1 greater than v's distance
-  //     Set predecessor to v
-  //     Enqueue w
-
-  while (queue.length > 0) {
-
-    var v = queue.dequeue();
-
-    if(fn && fn(v)) {
-      // early exit condition
-      break;
+    for (let i = 0; i <= graph.order(); i += 1) {
+      this.info[i] = {
+        distance: null,
+        predecessor: null,
+      };
     }
 
-    for (var j = 0; j < graph.adjacency_list[v].length; j++) {
-      var w = graph.adjacency_list[v][j];
+    this.info[startVertex].distance = 0;
 
-      if (this.info[w].distance === null) {
+    const queue = new Queue();
+    queue.enqueue(startVertex);
 
-        this.info[w].distance = this.info[v].distance + 1;
-        this.info[w].predecessor = v;
-        queue.enqueue(w);
+    // Traverse the graph
+
+    // As long as the queue is not empty:
+    //  Repeatedly dequeue a vertex v from the queue.
+    //
+    //  For each neighbor w of v that has not been visited:
+    //     Set distance to 1 greater than v's distance
+    //     Set predecessor to v
+    //     Enqueue w
+
+    while (queue.length > 0) {
+      const v = queue.dequeue();
+
+      if (fn && fn(v)) {
+        // early exit condition
+        break;
       }
 
+      for (let j = 0; j < graph.adjacency_list[v].length; j += 1) {
+        const w = graph.adjacency_list[v][j];
+
+        if (this.info[w].distance === null) {
+          this.info[w].distance = this.info[v].distance + 1;
+          this.info[w].predecessor = v;
+          queue.enqueue(w);
+        }
+      }
     }
 
+    return this;
   }
 
-  return this;
+  /**
+   * Reconstructs the path the traversal followed.
+   *
+   * Works by starting at the input goal vertex of this bfs and
+   *
+   * working back through its predecessor chain to the startVertex
+   *
+   * @param {any} goal vertex the bfs terminated in
+   * @returns {array}
+   * @memberof BFS
+   */
+  reconstructPath(goal) {
+    let current = goal;
+    const path = [current];
+
+    while (current !== this.startVertex) {
+      current = this.info[current].predecessor;
+      path.push(current);
+    }
+
+    return path.reverse();
+  }
 }
 
-BFS.prototype.reconstruct_path = function(goal){
-
-  var current = goal;
-  var path = [current];
-
-  while(current !== this.start_vertex){
-    current = this.info[current].predecessor;
-    path.push(current);
-  }
-
-  return path.reverse();
-};
-
-module.exports = BFS;
+export default BFS;
--- javascript/test/algorithms/graph/bfs_test.js
+++ javascript/test/algorithms/graph/bfs_test.js
@@ -1,12 +1,12 @@
-var assert = require('assert');
-var BFS = require('../../../algorithms/graph/bfs.js');
-var Graph = require('../../../data_structures/graph.js').default;
-var dsalgo = require('../../../utilities.js').default;
+import assert from 'assert';
+import BFS from '../../../algorithms/graph/bfs';
+import Graph from '../../../data_structures/graph';
 
-describe('Breadth First Search', function() {
-
-  describe('traverse graph', function() {
-    var adjListToo = [
+describe('Breadth First Search', () => {
+  describe('traverse graph', () => {
+    // https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/a/the-breadth-first-search-algorithm
+    // https://s3.amazonaws.com/ka-cs-algorithms/bfs_result.png
+    const adjListToo = [
       [1],
       [0, 4, 5],
       [3, 4, 5],
@@ -14,62 +14,59 @@ describe('Breadth First Search', function() {
       [1, 2],
       [1, 2, 6],
       [3, 5],
-      []
+      [],
     ];
 
-    var graph = new Graph({
-      adjList: adjListToo
+    const graph = new Graph({
+      adjList: adjListToo,
     });
 
-    var traversal = new BFS(graph, 3);
-    var bfsInfo = traversal.info;
+    const traversal = new BFS(graph, 3);
+    const bfsInfo = traversal.info;
 
-    it("marks source vertex's predecessor as null", function() {
+    it("marks source vertex's predecessor as null", () => {
       assert.deepEqual(bfsInfo[3], {
         distance: 0,
-        predecessor: null
+        predecessor: null,
       });
     });
 
-    it('goes in correct order', function() {
-
+    it('goes in correct order', () => {
       assert.deepEqual(bfsInfo[0], {
         distance: 4,
-        predecessor: 1
+        predecessor: 1,
       });
       assert.deepEqual(bfsInfo[1], {
         distance: 3,
-        predecessor: 4
+        predecessor: 4,
       });
       assert.deepEqual(bfsInfo[2], {
         distance: 1,
-        predecessor: 3
+        predecessor: 3,
       });
       assert.deepEqual(bfsInfo[4], {
         distance: 2,
-        predecessor: 2
+        predecessor: 2,
       });
       assert.deepEqual(bfsInfo[5], {
         distance: 2,
-        predecessor: 2
+        predecessor: 2,
       });
       assert.deepEqual(bfsInfo[6], {
         distance: 1,
-        predecessor: 3
+        predecessor: 3,
       });
     });
 
-    it('doesnt mark unconnected vertex', function() {
+    it('doesnt mark unconnected vertex', () => {
       assert.deepEqual(bfsInfo[7], {
         distance: null,
-        predecessor: null
+        predecessor: null,
       });
     });
 
-    it('traces a traversal path correctly', function() {
-      assert.deepEqual(traversal.reconstruct_path(0), [3,2,4,1,0]);
+    it('traces a traversal path correctly', () => {
+      assert.deepEqual(traversal.reconstructPath(0), [3, 2, 4, 1, 0]);
     });
-
   });
-
 });
