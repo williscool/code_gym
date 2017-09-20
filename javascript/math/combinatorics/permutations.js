cf87d8ca5ff8e88ab5aa14b7f715ea2daa0dad72
William Harris <wharris@upscalews.com>
2017-09-20 00:14:36 -0700

    es6ify and doc permutations

--- javascript/math/combinatorics/permutations.js
+++ javascript/math/combinatorics/permutations.js
@@ -1,37 +1,102 @@
-// http://en.wikipedia.org/wiki/Permutation
-// https://github.com/josdejong/mathjs/blob/master/lib/function/probability/permutations.js
-//
-// https://www.khanacademy.org/math/precalculus/prob_comb/combinatorics_precalc/v/permutation-formula
-//
-var factorial = require('../factorial.js').default.count_up_iterative;
-var dsalgo = require('../../utilities.js').default;
+import FactorialTypes from '../factorial';
+import dsalgo from '../../utilities';
 
-// TODO: this function should throw an error is k is higher than n
-// https://github.com/josdejong/mathjs/blob/master/lib/function/probability/combinations.js#L47
+/**
+ * Compute the number of ways of obtaining an ordered subset of `k` elements from a set of `n` elements.
+ *
+ * http://en.wikipedia.org/wiki/Permutation
+ *
+ * https://www.mathsisfun.com/combinatorics/combinations-permutations.html
+ *
+ * https://github.com/josdejong/mathjs/blob/master/lib/function/probability/permutations.js
+ *
+ * https://www.khanacademy.org/math/precalculus/prob_comb/combinatorics_precalc/v/permutation-formula
+ *
+ * @module Permutations
+ */
+const factorial = FactorialTypes.count_up_iterative;
+
+/**
+ * Permutations with repetition
+ * (often refered to as with replacement which I think is a poor choice of words and horribly confusing in this context)
+ *
+ * These are the easiest to calculate.
+ *
+ * When a thing has n different types ... we have n choices each time!
+ *
+ * We are doing that k times... thus n ^ k
+ *
+ *
+ * @param {number} n
+ * @param {number} k
+ * @returns {number}
+ */
+function withRep(n, k) {
+  return n ** k;
+}
+
+/**
+ * In permutations without repetition, we have to reduce the number of available choices each time.
+ *
+ * If we are not taking a subset k each time we can simply return `factorial(n)`
+ *
+ * If we a taking k... we want to remove the factorial of the remaining items we do so mathematically like so
+ *
+ * n!/(n - k)!
+ *
+ * @param {number} n
+ * @param {number} k
+ * @returns {number}
+ */
 function withoutRep(n, k) {
-
   if (!dsalgo.utils.isDefined(k)) return factorial(n);
 
-  if (k === 0) return 0;
+  if (k > n) {
+    throw new TypeError('second argument k must be an integer less than or equal to n');
+  }
 
-  var result = n;
-  var iteration = 1;
+  return Math.floor(factorial(n) / factorial(n - k));
+}
 
-  for (var i = k - 1; i > 0; i--) {
-    result = result * (n - iteration);
-    iteration++;
+/**
+ * There are several performance enhancements and improvements you'll see to the calculation
+ *
+ * of permutations without replacementmet in the wild
+ *
+ * discussed in this thread https://github.com/josdejong/mathjs/issues/134#issuecomment-3329019
+ *
+ * in the context of combinations but the optimized code for permutations with the same notions are used in math.js as well
+ *
+ * Simply put instead of calculating n and n - k's factorial's seperately
+ *
+ * then divide the former by the later
+ *
+ * calculate them at the same time
+ *
+ * with a for loop that runs from n - k to n while multiplying the result.
+ *
+ * @param {number} n
+ * @param {number} k
+ * @returns {number}
+ */
+function withoutRepOptimized(n, k) {
+  if (!dsalgo.utils.isDefined(k)) return factorial(n);
+
+  if (k > n) {
+    throw new TypeError('second argument k must be an integer less than or equal to n');
+  }
+
+  let result = 1;
+
+  for (let i = (n - k) + 1; i <= n; i += 1) {
+    result *= i;
   }
 
   return result;
 }
 
-// https://www.mathsisfun.com/combinatorics/combinations-permutations.html
-// with repetition (often refeered to as with replacement which I think is a poor choice of words and horribly confusing in this context)
-function withRep(n, k) {
-  return Math.pow(n, k);
-}
-
-module.exports = {
+export default {
+  with_repetition: withRep,
   without_repetition: withoutRep,
-  with_repetition: withRep
+  without_repetition_optimized: withoutRepOptimized,
 };
--- javascript/test/math/combinatorics/permutations_test.js
+++ javascript/test/math/combinatorics/permutations_test.js
@@ -1,29 +1,46 @@
-// inspired by https://github.com/josdejong/mathjs/blob/master/test/function/probability/permutation.test.js
-var assert = require('assert');
-var permutations = require('../../../math/combinatorics/permutations.js').without_repetition;
-describe("Permutations", function() {
-  it("should calculate the permutations of a number", function() {
-    assert.equal(permutations(0), 1);
-    assert.equal(permutations(1), 1);
-    assert.equal(permutations(2), 2);
-    assert.equal(permutations(3), 6);
-    assert.equal(permutations(4), 24);
-    assert.equal(permutations(5), 120);
-  });
-  it("should calculate the permutations of a number n choosing k at a time", function() {
-    assert.equal(permutations(5, 0), 0);
-    assert.equal(permutations(5, 1), 5);
-    assert.equal(permutations(5, 3), 60);
-    assert.equal(permutations(5, 4), 120);
-    assert.equal(permutations(9, 8), 362880);
-    assert.equal(permutations(7, 5), 2520);
+import assert from 'assert';
+import PermutationTypes from '../../../math/combinatorics/permutations';
+// inspired by https://github.com/josdejong/mathjs/blob/v3.16.3/test/function/probability/permutations.test.js
+
+const {
+  without_repetition: permutationsWithRep,
+  without_repetition_optimized: permutationsWithRepOpt,
+  with_repetition: permutationsWithRepetition,
+} = PermutationTypes;
+
+(['without_repetition', 'without_repetition_optimized']).forEach((key) => {
+  // iife
+  const permutations = (function functionPicker() {
+    if (key === 'without_repetition') {
+      return permutationsWithRep;
+    }
+    return permutationsWithRepOpt;
+  }());
+
+  describe(`Permutations ${key}`, () => {
+    it('should calculate the permutations of a number', () => {
+      assert.equal(permutations(0), 1);
+      assert.equal(permutations(1), 1);
+      assert.equal(permutations(2), 2);
+      assert.equal(permutations(3), 6);
+      assert.equal(permutations(4), 24);
+      assert.equal(permutations(5), 120);
+    });
+    it('should calculate the permutations of a number n choosing k at a time', () => {
+      assert.equal(permutations(5, 0), 1);
+      assert.equal(permutations(5, 1), 5);
+      assert.equal(permutations(5, 3), 60);
+      assert.equal(permutations(5, 4), 120);
+      assert.equal(permutations(9, 8), 362880);
+      assert.equal(permutations(7, 5), 2520);
+    });
   });
 });
-var permutations_with_repetition = require('../../../math/combinatorics/permutations.js').with_repetition;
-describe("Permutations with Repetition", function() {
-  it("should calculate the permutations of a number n choosing k at a time with repetition", function() {
-    assert.equal(permutations_with_repetition(5, 0), 1);
-    assert.equal(permutations_with_repetition(5, 1), 5);
-    assert.equal(permutations_with_repetition(5, 2), 25);
+
+describe('Permutations with Repetition', () => {
+  it('should calculate the permutations of a number n choosing k at a time with repetition', () => {
+    assert.equal(permutationsWithRepetition(5, 0), 1);
+    assert.equal(permutationsWithRepetition(5, 1), 5);
+    assert.equal(permutationsWithRepetition(5, 2), 25);
   });
 });
