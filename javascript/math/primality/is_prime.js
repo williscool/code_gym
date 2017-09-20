159ce329c745ab506a70100bf490c9b20df11e90
William Harris <wharris@upscalews.com>
2017-09-20 02:22:53 -0700

    es6ify is prime.
    was already pretty well documented.
    
    plus its pretty straightfoward

--- javascript/math/primality/is_prime.js
+++ javascript/math/primality/is_prime.js
@@ -1,51 +1,74 @@
-// https://en.wikipedia.org/wiki/Primality_test#Simple_methods
+/**
+ * https://en.wikipedia.org/wiki/Primality_test#Simple_methods
+ *
+ * why 0 isnt prime
+ * http://mathforum.org/library/drmath/view/57036.html
+ *
+ * why 1 isn't prime
+ * https://www.youtube.com/watch?v=IQofiPqhJ_s
+ *
+ * also negative numbers either are or arent depending on context. I'm saying they arent
+ *
+ * https://primes.utm.edu/notes/faq/negative_primes.html
+ * http://math.stackexchange.com/questions/1002459/do-we-have-negative-prime-numbers
+ * http://www.quora.com/Why-dont-we-have-negative-prime-numbers
+ *
+ * @module isPrime
+ */
 
-// inspired by: http://www.thatjsdude.com/interview/js1.html#isPrime
+/**
+ * Dead simple. Literally check every number 2 and greater less than n
+ *
+ * and see if n is evenly divisble by them
+ *
+ * inspired by: http://www.thatjsdude.com/interview/js1.html#isPrime
+ *
+ * @param {number} n
+ * @returns {boolean}
+ */
 function naiveTrialDivision(n) {
-  //
-  // why 0 isnt prime
-  // http://mathforum.org/library/drmath/view/57036.html
-  //
-  // why 1 isn't prime
-  // https://www.youtube.com/watch?v=IQofiPqhJ_s
-  //
-  // also negative numbers either are or arent depending on context. I'm saying they arent
-  //
-  // https://primes.utm.edu/notes/faq/negative_primes.html
-  // http://math.stackexchange.com/questions/1002459/do-we-have-negative-prime-numbers
-  // http://www.quora.com/Why-dont-we-have-negative-prime-numbers
-
   if (n < 2) return false;
 
-  var divisor = 2;
+  let divisor = 2;
 
   while (n > divisor) {
     if (n % divisor === 0) {
       return false;
-    } else {
-      divisor = divisor + 1;
     }
+    divisor += 1;
   }
 
   return true;
 }
 
-// inspried by http://www.thatjsdude.com/interview/js1.html#primeExtra
-// https://github.com/mgechev/javascript-algorithms/blob/master/src/primes/is-prime.js
-// https://en.wikipedia.org/wiki/Primality_test#Pseudocode
-//
-// basically the improvements to this test boil down to the intuition that if you can't evenly divide the number we are testing by 2. 
-// clearly you only need to look at the numbers lower than half of its value. 
-//
-// If you can't evenly divide it by 3 you only need to test numbers under 1/3 of its value... and so on and so forth
-//
-// from the wikipedia article:
-// The algorithm can be improved further by observing that all primes are of the form 6k ± 1, with the exception of 2 and 3. 
-// This is because all integers can be expressed as (6k + i) for some integer k and for i = -1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4); and 3 divides (6k + 3). 
-// So a more efficient method is to test if n is divisible by 2 or 3, then to check through all the numbers of form 6k ± 1 <= sqrt(n).
-//
+/**
+ * Basically the improvements to this test boil down to the intuition that
+ *
+ * if you can't evenly divide the number n we are testing by 2...
+ *
+ * clearly you only need to look at the numbers lower than half of n's value.
+ *
+ * If you can't evenly divide n by 3 you only need to test numbers under 1/3 of n's value... and so on and so forth
+ *
+ * from the wikipedia article:
+ *
+ * "The algorithm can be improved further by observing that all primes are of the form 6k ± 1, with the exception of 2 and 3.
+ *
+ * This is because all integers can be expressed as
+ *
+ * (6k + i) for some integer k and for i = -1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4); and 3 divides (6k + 3).
+ *
+ * So a more efficient method is to test if n is divisible by 2 or 3, then to check through all the numbers of form 6k ± 1 <= sqrt(n).""
+ *
+ * inspired by:
+ * http://www.thatjsdude.com/interview/js1.html#primeExtra
+ * https://github.com/mgechev/javascript-algorithms/blob/master/src/primes/is-prime.js
+ * https://en.wikipedia.org/wiki/Primality_test#Pseudocode
+ *
+ * @param {number} n
+ * @returns {boolean}
+ */
 function optimizedTrialDivision(n) {
-
   if (n < 2) return false;
 
   if (n <= 3) return true;
@@ -53,8 +76,8 @@ function optimizedTrialDivision(n) {
   if ((n % 2 === 0) || (n % 3 === 0)) return false;
 
   // divisor * divisor <= n
-  var divisor = 5;
-  var limit = Math.floor(Math.sqrt(n));
+  let divisor = 5;
+  const limit = Math.floor(Math.sqrt(n));
 
   while (divisor <= limit) {
     if ((n % divisor === 0) || (n % (divisor + 2) === 0)) return false;
@@ -64,12 +87,12 @@ function optimizedTrialDivision(n) {
   return true;
 }
 
-// more versions 
+// more versions
 // https://www.topcoder.com/community/data-science/data-science-tutorials/primality-testing-non-deterministic-algorithms/
 // https://en.wikipedia.org/wiki/Primality_test#Probabilistic_tests
 // https://en.wikipedia.org/wiki/Primality_test#Fast_deterministic_tests
 
-module.exports = {
+export default {
   naive_trial_division: naiveTrialDivision,
-  optimized_trial_division: optimizedTrialDivision
+  optimized_trial_division: optimizedTrialDivision,
 };
--- javascript/test/math/primality/is_prime_test.js
+++ javascript/test/math/primality/is_prime_test.js
@@ -1,12 +1,12 @@
-var assert = require('assert');
-var isPrimeFns = require('../../../math/primality/is_prime.js');
+import assert from 'assert';
+import isPrimeFns from '../../../math/primality/is_prime';
 
-Object.keys(isPrimeFns).forEach(function(key) {
-  var isPrime = isPrimeFns[key];
-  describe(key + " isPrime", function() {
-    it("should calculate if a number is prime", function() {
+Object.keys(isPrimeFns).forEach((key) => {
+  const isPrime = isPrimeFns[key];
+  describe(`${key} isPrime`, () => {
+    it('should calculate if a number is prime', () => {
       assert.equal(isPrime(0), false);
-      assert.equal(isPrime(1), false); // because math https://primes.utm.edu/notes/faq/one.html 
+      assert.equal(isPrime(1), false); // because math https://primes.utm.edu/notes/faq/one.html
       assert.equal(isPrime(2), true);
       assert.equal(isPrime(3), true);
       assert.equal(isPrime(4), false);
