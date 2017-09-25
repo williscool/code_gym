0f954b9f7ffe3a7620dbf805b5bf7faa31f8314f
William Harris <wharris@upscalews.com>
2017-09-25 01:54:19 -0700

    es6ify and doc bit flipper

--- javascript/algorithms/bit_manipulation/flippin_bits.js
+++ javascript/algorithms/bit_manipulation/flippin_bits.js
@@ -1,16 +1,27 @@
-// https://en.wikipedia.org/wiki/Bit_manipulation
-// https://www.youtube.com/watch?v=uUtb0BaeosQ
-//
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
-// https://www.topcoder.com/community/data-science/data-science-tutorials/a-bit-of-fun-fun-with-bits/
-//
-// https://en.wikipedia.org/wiki/Two%27s_complement#Converting_from_two.27s_complement_representation
+import dsalgo from '../../utilities';
+// bit manipulation is litterally the entire point of this module
+/* eslint-disable no-bitwise  */
 
-var dsalgo = require('../../utilities.js').default;
-
-module.exports = function(a, b) {
-
-  var flips = {};
+/**
+ * Literally just a function that takes an a and b and does bit manipulations with the 2
+ *
+ * Pretty useless for all other purposes outside of learning
+ *
+ * https://en.wikipedia.org/wiki/Bit_manipulation
+ * https://www.youtube.com/watch?v=uUtb0BaeosQ
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
+ * https://www.topcoder.com/community/data-science/data-science-tutorials/a-bit-of-fun-fun-with-bits/
+ *
+ * https://en.wikipedia.org/wiki/Two%27s_complement#Converting_from_two.27s_complement_representation
+ *
+ * @export
+ * @param {number} a
+ * @param {number} b
+ * @returns {Object}
+ */
+export default function (a, b) {
+  const flips = {};
 
   flips.binarya = dsalgo.utils.dec2bin(a);
   flips.binaryb = dsalgo.utils.dec2bin(b);
@@ -25,8 +36,11 @@ module.exports = function(a, b) {
   flips.rightshifta_by_b = a >> b;
   flips.leftshifta_by_b = a << b;
 
+  /**
+   * Unlike an arithmetic shift, a logical shift does not preserve a number's sign bit or distinguish a number's exponent from its significand (mantissa);
+   * every bit in the operand is simply moved a given number of bit positions, and the vacant bit-positions are filled, usually with zeros.
+   */
   flips.logicalrightshifta_by_b = a >>> b;
 
   return flips;
-
-};
+}
--- javascript/test/algorithms/bit_manipulation/flippin_bits_test.js
+++ javascript/test/algorithms/bit_manipulation/flippin_bits_test.js
@@ -1,45 +1,45 @@
-var assert = require('assert');
-var dsalgo = require('../../../utilities.js').default;
-var flippinBits = require('../../../algorithms/bit_manipulation/flippin_bits.js');
+import assert from 'assert';
+import dsalgo from '../../../utilities';
+import FlippinBits from '../../../algorithms/bit_manipulation/flippin_bits';
 
-describe("Flipping Some Bits", function() {
-  var fb = flippinBits(2, 4);
+describe('Flipping Some Bits', () => {
+  const fb = FlippinBits(2, 4);
 
-  it("Binary Representation", function() {
-    assert.equal(fb.binarya, "10");
-    assert.equal(fb.binaryb, "100");
+  it('Binary Representation', () => {
+    assert.equal(fb.binarya, '10');
+    assert.equal(fb.binaryb, '100');
   });
 
-  it("Not", function() {
-    assert.deepEqual(dsalgo.utils.dec2bin(fb.nota), "11111111111111111111111111111101");
-    assert.deepEqual(dsalgo.utils.dec2bin(fb.notb), "11111111111111111111111111111011");
+  it('Not', () => {
+    assert.deepEqual(dsalgo.utils.dec2bin(fb.nota), '11111111111111111111111111111101');
+    assert.deepEqual(dsalgo.utils.dec2bin(fb.notb), '11111111111111111111111111111011');
   });
 
-  it("And", function() {
+  it('And', () => {
     assert.equal(fb.and, 0);
   });
 
-  it("Or", function() {
+  it('Or', () => {
     assert.equal(fb.or, 6);
   });
 
-  it("Xor", function() {
+  it('Xor', () => {
     assert.equal(fb.or, 6);
   });
 
-  it("right shift", function() {
-    var fb = flippinBits(4, 2);
-    assert.equal(fb.rightshifta_by_b, 1);
+  it('right shift', () => {
+    const diffb = FlippinBits(4, 2);
+    assert.equal(diffb.rightshifta_by_b, 1);
   });
 
-  it("left shift", function() {
-    var fb = flippinBits(4, 2);
-    assert.equal(fb.leftshifta_by_b, 16);
+  it('left shift', () => {
+    const diffb = FlippinBits(4, 2);
+    assert.equal(diffb.leftshifta_by_b, 16);
   });
 
-  it("logical right shift", function() {
-    var fb = flippinBits(-8, 1);
+  it('logical right shift', () => {
+    const bitFlipper = FlippinBits(-8, 1);
     // leading zeros get dropped in the conversion
-    assert.equal(dsalgo.utils.dec2bin(fb.logicalrightshifta_by_b), "1111111111111111111111111111100");
+    assert.equal(dsalgo.utils.dec2bin(bitFlipper.logicalrightshifta_by_b), '1111111111111111111111111111100');
   });
 });
