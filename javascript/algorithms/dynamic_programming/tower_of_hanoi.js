e8a6e4efc74379519693090adeb5b3f8a4c95966
William Harris <wharris@upscalews.com>
2017-09-25 00:25:44 -0700

    es6ify hanoi

--- javascript/algorithms/dynamic_programming/tower_of_hanoi.js
+++ javascript/algorithms/dynamic_programming/tower_of_hanoi.js
@@ -1,35 +1,54 @@
-// https://en.wikipedia.org/wiki/Tower_of_Hanoi
-// https://en.wikipedia.org/wiki/Dynamic_programming#Tower_of_Hanoi_puzzle
-// https://proofwiki.org/wiki/Tower_of_Hanoi
-//
-// my own personal hang up with this problem at first is that unlike many of the other dp solutions for problems 
-// the classical solution to the tower of  is exponential in time complexity as opposed to linear such as memoized nth fibbonaci) 
-//
-// however a clever professor Eastern Washington University has figured out how to trade exponential time for exponential space complexity o.o
-// here http://penguin.ewu.edu/~trolfe/DynamicHanoi/
-//
-// if you implement this let me know. It would be interesting to talk about it
-//
-//
-// inspired by:
-// https://www.khanacademy.org/computing/computer-science/algorithms/towers-of-hanoi/p/challenge-solve-hanoi-recursively
-// https://github.com/mgechev/javascript-algorithms/blob/master/src/others/hanoi.js
-// http://www2.hh.se/staff/vero/itads/2007/lectures/lecture4/printL4.pdf
-// http://introcs.cs.princeton.edu/java/23recursion/TowersOfHanoi.java.html
 
-function Hanoi(num_disks, source, destination, spare, result) {
-
-  // if(num_disks === 0) // do nothing
-
-  result = result || [];
-
-  if (num_disks > 0) {
-    Hanoi(num_disks - 1, source, spare, destination, result);
+/**
+ * https://en.wikipedia.org/wiki/Tower_of_Hanoi
+ *
+ * Likely never TODO:
+ *
+ * reimplement with this solution. So I can see how the disks move back and forth
+ *
+ * which would probably help me really understand wtf is goin on here
+ *
+ *  https://en.wikipedia.org/wiki/Tower_of_Hanoi#Recursive_solution
+ *
+ * also this video is pretty helpful https://www.youtube.com/watch?v=UuIneNBbscc
+ *
+ * What I do get so far is that there are 2^n (where n is the num of disks)
+ * states the game can be in and at the start you are in one... so obviously to get to this other state we will need to (2^n) - 1 movees
+ * notes:
+ *
+ * https://en.wikipedia.org/wiki/Dynamic_programming#Tower_of_Hanoi_puzzle
+ * https://proofwiki.org/wiki/Tower_of_Hanoi
+ *
+ * my own personal hang up with this problem at first is that unlike many of the other dp solutions for problems
+ * the classical solution to the tower of is exponential in time complexity as opposed to linear such as memoized nth fibbonaci)
+ *
+ * however a clever professor Eastern Washington University has figured out how to trade exponential time for exponential space complexity o.o
+ * here http://penguin.ewu.edu/~trolfe/DynamicHanoi/
+ *
+ * if you implement this let me know. It would be interesting to talk about it
+ *
+ * inspired by:
+ * https://www.khanacademy.org/computing/computer-science/algorithms/towers-of-hanoi/p/challenge-solve-hanoi-recursively
+ * https://github.com/mgechev/javascript-algorithms/blob/master/src/others/hanoi.js
+ * http://www2.hh.se/staff/vero/itads/2007/lectures/lecture4/printL4.pdf
+ * http://introcs.cs.princeton.edu/java/23recursion/TowersOfHanoi.java.html
+ *
+ * @param {number} numDisks
+ * @param {number} source
+ * @param {number} destination
+ * @param {number} spare
+ * @param {number[]} [result=[]]
+ * @returns {array}
+ */
+function Hanoi(numDisks, source, destination, spare, result = []) {
+  // if(numDisks === 0) // do nothing
+  if (numDisks > 0) {
+    Hanoi(numDisks - 1, source, spare, destination, result);
     result.push([source, destination]);
-    Hanoi(num_disks - 1, spare, destination, source, result);
+    Hanoi(numDisks - 1, spare, destination, source, result);
   }
 
   return result;
 }
 
-module.exports = Hanoi;
+export default Hanoi;
--- javascript/test/algorithms/dynamic_programming/tower_of_hanoi_test.js
+++ javascript/test/algorithms/dynamic_programming/tower_of_hanoi_test.js
@@ -1,24 +1,23 @@
-// inspired by: https://github.com/josdejong/mathjs/blob/master/test/function/probability/factorial.test.js
+import assert from 'assert';
+import toh from '../../../algorithms/dynamic_programming/tower_of_hanoi';
 
-var assert = require('assert');
-var toh = require('../../../algorithms/dynamic_programming/tower_of_hanoi.js');
-describe("Tower of Hanoi", function() {
-  it("3 disks", function() {
-    var n = 3;
-    var moves = toh(n, 1, 2, 3);
-    // console.log(moves);
+describe('Tower of Hanoi', () => {
+  it('3 disks', () => {
+    const n = 3;
+    const moves = toh(n, 1, 2, 3);
+    console.log(moves);
     // console.log(moves.length);
     //
     // TODO: test the actual moves
-    assert.equal(moves.length, Math.pow(2, n) - 1);
+    assert.equal(moves.length, (2 ** n) - 1);
   });
-  it("5 disks", function() {
-    var n = 5;
-    var moves = toh(n, 1, 2, 3);
+  it('5 disks', () => {
+    const n = 5;
+    const moves = toh(n, 1, 2, 3);
     // console.log(moves);
     // console.log(moves.length);
     //
     // TODO: test the actual moves
-    assert.equal(moves.length, Math.pow(2, n) - 1);
+    assert.equal(moves.length, (2 ** n) - 1);
   });
 });
